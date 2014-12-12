3aa40a35a5aea3857db716b80b2222ba981f0cb4
pldin601 <roman@homefs.biz>
2014-12-12 20:40:19 +0200

    REST Development

--- index.php
+++ index.php
@@ -4,29 +4,34 @@ include "libs/FluentPDO/FluentPDO.php";
 
 require_once "application/startup.php";
 
+$sr = new SuperRouter();
+
+$sr->when("/cool", function () {
+    echo 'Hello, World!';
+});
+
+
 $baseMemory = memory_get_usage();
 $sTime = microtime(true);
 
 user::loginBySession();
 
-if(application::getMethod() === "POST")
-{
+if (application::getMethod() === "POST") {
     header("Content-Type: application/json; charset=UTF-8");
 }
 
-try 
-{
-    $ctrl = new router(); $ctrl->start(); unset($ctrl);
-}
-catch(morException $ex)
-{
+try {
+    $ctrl = new router();
+    $ctrl->start();
+    unset($ctrl);
+} catch (morException $ex) {
     echo $ex->getErrorPage();
 }
 
 $newMemory = memory_get_usage();
 
-misc::writeDebug(sprintf("%s: memory(%0.2f MB) time(%0.2f s)", 
-        application::get("route", "", REQ_STRING), 
-        ($newMemory - $baseMemory) / 1000000, microtime(true) - $sTime));
+misc::writeDebug(sprintf("%s: memory(%0.2f MB) time(%0.2f s)",
+    application::get("route", "", REQ_STRING),
+    ($newMemory - $baseMemory) / 1000000, microtime(true) - $sTime));
 
 

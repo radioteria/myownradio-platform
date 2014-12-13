f84e05c3e3bf13d5e49c53b5821379d478f858eb
pldin601 <GDk4F/so>
2014-12-13 19:32:44 +0200

    Intensive Refactoring

--- application/classes/MVC/Router.php
+++ application/classes/MVC/Router.php
@@ -42,7 +42,7 @@ class Router {
     private function loadDependencies(array $params) {
         $dependencies = [];
         foreach ($params as $param) {
-            echo $param->getClass();
+            print_r($param->getClass());
         }
     }
 

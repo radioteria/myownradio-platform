4c58f463d3da6b64b4eac5f46707aaea4b05b955
pldin601 <GDk4F/so>
2014-12-13 19:44:56 +0200

    Intensive Refactoring

--- application/classes/MVC/Router.php
+++ application/classes/MVC/Router.php
@@ -49,7 +49,7 @@ class Router {
     private function isSingleton(\ReflectionClass $class) {
         $traits = $class->getTraits();
         foreach($traits as $trait) {
-            echo $trait->getName() . "<br />";
+            echo $trait->getShortName() . "<br />";
             if ($trait->getName() === "Singleton") return true;
         }
         return false;

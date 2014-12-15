0b0df6c2cdec3a1c17bb9e0bade9bf99310c6503
pldin601 <GDk4F/so>
2014-12-15 20:15:56 +0200

    Intensive Refactoring

--- application/classes/MVC/Controllers/api/v2/stream/DoCreate.php
+++ application/classes/MVC/Controllers/api/v2/stream/DoCreate.php
@@ -20,11 +20,11 @@ class DoCreate extends Controller {
     public function doPost(HttpPost $post, InputValidator $validator, Fabric $fabric, JsonResponse $response) {
 
         // Get user input parameters
-        $name = $post->getParameter("name")->getOrElseThrow(ControllerException::noArgument("name"));
-        $info = $post->getParameter("info")->getOrElseEmpty();
-        $tags = $post->getParameter("tags")->getOrElseEmpty();
-        $permalink = $post->getParameter("permalink")->getOrElseNull();
-        $category = $post->getParameter("category")->getOrElseNull();
+        $name       = $post->getParameter("name")       ->getOrElseThrow(ControllerException::noArgument("name"));
+        $info       = $post->getParameter("info")       ->getOrElseEmpty();
+        $tags       = $post->getParameter("tags")       ->getOrElseEmpty();
+        $permalink  = $post->getParameter("permalink")  ->getOrElseNull();
+        $category   = $post->getParameter("category")   ->getOrElseNull();
 
         // Validate parameters
         $validator->validateStreamName($name);

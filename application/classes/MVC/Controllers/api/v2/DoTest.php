4cf085ad7f450f4f8e779a43014a30b223f2b960
pldin601 <roman@homefs.biz>
2014-12-16 11:10:32 +0200

    REST Development

--- application/classes/MVC/Controllers/api/v2/DoTest.php
+++ application/classes/MVC/Controllers/api/v2/DoTest.php
@@ -11,9 +11,24 @@ namespace MVC\Controllers\api\v2;
 
 use MVC\Controller;
 use MVC\Services\HttpGet;
+use MVC\Services\JsonResponse;
 
 class DoTest extends Controller {
-    public function doGet(HttpGet $get) {
+    public function doGet(HttpGet $get, JsonResponse $response) {
+
+        $get->getParameter("id")
+
+            ->then(function ($id) use ($response) {
+
+                $response->setMessage("ID = " . $id);
+
+            })
+
+            ->otherwise(function () use ($response) {
+
+                $response->setMessage("No ID :(");
+
+            });
 
     }
 } 

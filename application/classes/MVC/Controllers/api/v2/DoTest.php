45bb6fb8213258ce4732119f7116e31e8deb1d71
pldin601 <roman@homefs.biz>
2014-12-17 14:37:32 +0200

    Implemented 'Do In Transaction' pattern, created 'DBQuery' classes.

--- application/classes/MVC/Controllers/api/v2/DoTest.php
+++ application/classes/MVC/Controllers/api/v2/DoTest.php
@@ -11,19 +11,18 @@ namespace MVC\Controllers\api\v2;
 
 use MVC\Controller;
 use MVC\Services\Database;
-use MVC\Services\DB\Query\InsertQuery;
 use MVC\Services\JsonResponse;
 
 class DoTest extends Controller {
     public function doGet(JsonResponse $response) {
 
-        $pdo = Database::getInstance()->getPDO();
+        $track = Database::doInTransaction(function (Database $db) {
 
-        $query = new InsertQuery("r_streams");
-        $query->values("access", 1);
+            return $db->fetchOneRow("SELECT 1")->getOrElse("No track");
 
-        $response->setMessage($query->getQuery($pdo));
-        $response->setData($query->getParameters());
+        });
+
+        $response->setData($track);
 
     }
 } 

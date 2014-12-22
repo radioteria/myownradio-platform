dd6f4775e8ccc833f7dc0aee8de72bf59f38c509
pldin601 <roman@homefs.biz>
2014-12-22 11:37:52 +0200

    Injecting ORM into Model Entities

---
+++ application/classes/Framework/Controllers/api/v2/streams/DoGetBookmarks.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roman
+ * Date: 22.12.14
+ * Time: 11:36
+ */
+
+namespace Framework\Controllers\api\v2\streams;
+
+
+class DoGetBookmarks {
+
+    public function doGet() {
+        echo '123';
+    }
+
+} 

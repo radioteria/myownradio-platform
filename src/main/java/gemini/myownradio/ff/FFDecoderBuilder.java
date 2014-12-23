2e09676cf61b746ce4f0b3befa3a41351501ae96
pldin601 <GDk4F/so>
2014-12-23 22:33:21 +0200

    Added new feature: skip track if file not exists

--- src/main/java/gemini/myownradio/ff/FFDecoderBuilder.java
+++ src/main/java/gemini/myownradio/ff/FFDecoderBuilder.java
@@ -14,34 +14,31 @@ public class FFDecoderBuilder {
         this.filename = filename;
         this.offset = offset;
 
-        if (jingled) {
-            cmd = new String[]{
-                    "ffmpeg",
-                    "-ss", new DecimalFormat("0.###").format((float) this.offset / 1000F),
-                    "-i", this.filename,
-                    "-i", "/media/www/myownradio.biz/jingle.wav",
-                    "-filter_complex", "[0:a]afade=t=in:ss=0:st=1:d=3[a1],[a1]amix=inputs=2:duration=first:dropout_transition=3",
-                    "-vn",
-                    "-acodec", "pcm_s16le",
-                    "-ar", "44100",
-                    "-ac", "2",
-                    "-f", "s16le",
-                    "-"
-            };
-        } else {
-            cmd = new String[]{
-                    "ffmpeg",
-                    "-ss", new DecimalFormat("0.###").format((float) this.offset / 1000F),
-                    "-i", this.filename,
-                    "-filter", "afade=t=in:ss=0:st=0:d=1",
-                    "-vn",
-                    "-acodec", "pcm_s16le",
-                    "-ar", "44100",
-                    "-ac", "2",
-                    "-f", "s16le",
-                    "-"
-            };
-        }
+        cmd = jingled ?
+                new String[]{
+                        "ffmpeg",
+                        "-ss", new DecimalFormat("0.###").format((float) this.offset / 1000F),
+                        "-i", this.filename,
+                        "-i", "/media/www/myownradio.biz/jingle.wav",
+                        "-filter_complex", "[0:a]afade=t=in:ss=0:st=1:d=3[a1],[a1]amix=inputs=2:duration=first:dropout_transition=3",
+                        "-vn",
+                        "-acodec", "pcm_s16le",
+                        "-ar", "44100",
+                        "-ac", "2",
+                        "-f", "s16le",
+                        "-"} :
+                new String[]{
+                        "ffmpeg",
+                        "-ss", new DecimalFormat("0.###").format((float) this.offset / 1000F),
+                        "-i", this.filename,
+                        "-filter", "afade=t=in:ss=0:st=0:d=1",
+                        "-vn",
+                        "-acodec", "pcm_s16le",
+                        "-ar", "44100",
+                        "-ac", "2",
+                        "-f", "s16le",
+                        "-"
+                };
     }
 
     public String[] generate() {

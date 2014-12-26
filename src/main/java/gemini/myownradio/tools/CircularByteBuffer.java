450f8cda376eb09a83670fe9f6a7cb789fbc96af
pldin601 <roman@homefs.biz>
2014-12-26 16:02:32 +0200

    Optimizing

--- src/main/java/gemini/myownradio/tools/CircularByteBuffer.java
+++ src/main/java/gemini/myownradio/tools/CircularByteBuffer.java
@@ -1,5 +1,6 @@
 package gemini.myownradio.tools;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 /**
@@ -11,17 +12,33 @@ public class CircularByteBuffer {
     private ByteBuffer buffer;
     private int length;
 
-    public CircularByteBuffer(int size) {
+    // Default read timeout is 5 seconds.
+    private final static long DEFAULT_TIMEOUT = 5_000L;
+
+    private long timeout;
+
+    public CircularByteBuffer(int size, long timeout) {
         this.count = 0L;
         this.buffer = ByteBuffer.allocateDirect(size);
         this.length = size;
+        this.timeout = timeout;
+    }
+
+    public CircularByteBuffer(int size) {
+        this(size, DEFAULT_TIMEOUT);
     }
 
     public void putBytes(byte[] b, int pos, int len) {
+
         buffer.position(len);
         buffer.compact();
         buffer.put(b, pos, len);
         count =+ len;
+
+        synchronized (this) {
+            this.notifyAll();
+        }
+
     }
 
     public void putBytes(byte[] b) {
@@ -34,6 +51,45 @@ public class CircularByteBuffer {
 
     public int getLength() {
         return length;
+    }
+
+    public long read(long after, byte[] b, int len) throws IOException {
+
+        // Will read buffer contents which written after specified position.
+        // In case of no data written after specified position method will
+        // be blocked until new data arrive.
+
+        long threshold = System.currentTimeMillis() + timeout;
+
+        while (threshold < System.currentTimeMillis()) {
+
+            if (count < after) {
+
+                synchronized (this) {
+                    try { this.wait(timeout); }
+                    catch (InterruptedException cannotHappen) { /* NOP */ }
+                }
+
+            } else {
+
+                int newBytes = (int) (count - after);
+
+                buffer.rewind();
+
+                if (newBytes > len) {
+                    buffer.get(b, 0, len);
+                    return count + len;
+                } else {
+                    buffer.get(b, 0, newBytes);
+                    return count + newBytes;
+                }
+
+            }
+
+        }
+
+        throw new IOException("Buffer wait timeout");
+
     }
 
 }

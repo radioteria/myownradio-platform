06e909f716857afa2f5e25a9ed5117ea8204c4da
pldin601 <roman@homefs.biz>
2014-12-26 15:39:30 +0200

    Optimizing

---
+++ src/main/java/gemini/myownradio/tools/CircularByteBuffer.java
@@ -0,0 +1,46 @@
+package gemini.myownradio.tools;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by Roman on 26.12.2014.
+ */
+public class CircularByteBuffer {
+
+    private long count;
+    private ByteBuffer buffer;
+    private int length;
+
+    public CircularByteBuffer(int size) {
+        this.count = 0L;
+        this.buffer = ByteBuffer.allocateDirect(size);
+        this.length = size;
+    }
+
+    public void putBytes(byte[] b, int pos, int len) {
+        buffer.position(len);
+        buffer.compact();
+        buffer.put(b, pos, len);
+        count =+ len;
+    }
+
+    public void putByte(byte b) {
+        buffer.position(1);
+        buffer.compact();
+        buffer.put(b);
+        count ++;
+    }
+
+    public void putBytes(byte[] b) {
+        this.putBytes(b, 0, b.length);
+    }
+
+    public long getPosition() {
+        return count;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+}

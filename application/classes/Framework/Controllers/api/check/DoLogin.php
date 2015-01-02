0e155003c5856338fbfeaff0cde8581dccae0c5d
pldin601 <GDk4F/so>
2015-01-02 22:29:04 +0200

    Refactoring

--- application/classes/Framework/Controllers/api/check/DoLogin.php
+++ application/classes/Framework/Controllers/api/check/DoLogin.php
@@ -10,14 +10,21 @@ namespace Framework\Controllers\api\check;
 
 
 use Framework\Controller;
+use Framework\Preferences;
 use Framework\Services\DB\DBQuery;
 use Framework\Services\HttpPost;
 use Framework\Services\JsonResponse;
 
 class DoLogin implements Controller {
-    public function doPost(HttpPost $post, JsonResponse $response, DBQuery $query) {
+    public function doPost(HttpPost $post, JsonResponse $response, DBQuery $query, Preferences $preferences) {
         $field = $post->getRequired("field");
-        $count = !boolval(count($query->selectFrom("r_users")->where("login", $field)));
-        $response->setData(["available" => $count]);
+
+        if (array_search($field, $preferences->get("invalid", "login")->get()) !== false) {
+            $response->setData(["available" => false]);
+        } else {
+            $count = !boolval(count($query->selectFrom("r_users")->where("login", $field)));
+            $response->setData(["available" => $count]);
+        }
+
     }
 } 

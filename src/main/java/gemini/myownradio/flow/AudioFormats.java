94feb24f5204f0eb1310981dcb182e7a3df57341
pldin601 <GDk4F/so>
2015-01-06 20:22:11 +0200

    CircularByteBuffer

--- src/main/java/gemini/myownradio/flow/AudioFormats.java
+++ src/main/java/gemini/myownradio/flow/AudioFormats.java
@@ -4,11 +4,11 @@ package gemini.myownradio.flow;
  * Created by Roman on 14.10.14.
  */
 public enum AudioFormats {
-    aacplus_24k(24000, "libfdk_aac", "flv", "video/x-flv"),
-    aacplus_32k(32000, "libfdk_aac", "flv", "video/x-flv"),
-    aacplus_64k(64000, "libfdk_aac", "flv", "video/x-flv"),
-    aacplus_96k(96000, "libfdk_aac", "flv", "video/x-flv"),
-    aacplus_128k(128000, "libfdk_aac", "flv", "video/x-flv"),
+    aacplus_24k(24000, "libfdk_aac", "adts", "audio/aac"),
+    aacplus_32k(32000, "libfdk_aac", "adts", "audio/aac"),
+    aacplus_64k(64000, "libfdk_aac", "adts", "audio/aac"),
+    aacplus_96k(96000, "libfdk_aac", "adts", "audio/aac"),
+    aacplus_128k(128000, "libfdk_aac", "adts", "video/x-flv"),
     mp3_128k(128000, "libmp3lame", "mp3", "audio/mpeg"),
     mp3_256k(256000, "libmp3lame", "mp3", "audio/mpeg");
 

9be1bca587be4d01673b17c30b5f32eb34e634c5
pldin601 <roman@homefs.biz>
2015-02-10 14:58:25 +0200

    Added HTTP Headers object

---
+++ application/classes/Framework/Services/HttpHeaders.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roman
+ * Date: 10.02.15
+ * Time: 9:51
+ */
+
+namespace Framework\Services;
+
+
+use Tools\Optional;
+use Tools\Singleton;
+use Tools\SingletonInterface;
+
+class HttpHeaders extends HttpRequestAdapter implements Injectable, SingletonInterface {
+
+    use Singleton;
+
+    private $headers;
+
+    function __construct() {
+        $this->headers = getallheaders();
+    }
+
+    public function getParameter($key) {
+        return Optional::ofEmpty(@$this->headers[$key]);
+    }
+
+} 

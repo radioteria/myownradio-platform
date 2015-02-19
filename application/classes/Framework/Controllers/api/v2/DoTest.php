e93508fb01f559e60c8a74da2a60807631a718a3
pldin601 <GDk4F/so>
2015-02-19 21:19:54 +0200

    etc

--- application/classes/Framework/Controllers/api/v2/DoTest.php
+++ application/classes/Framework/Controllers/api/v2/DoTest.php
@@ -10,11 +10,9 @@ namespace Framework\Controllers\api\v2;
 
 
 use Framework\Controller;
-use Framework\Models\AuthUserModel;
 use Framework\Services\JsonResponse;
 
 class DoTest implements Controller {
-    public function doGet(JsonResponse $response, AuthUserModel $model) {
-        $response->setData($model->getCurrentPlan());
+    public function doGet(JsonResponse $response) {
     }
 } 

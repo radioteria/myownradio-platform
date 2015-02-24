9614ecb4e5be773acfe3b27187acbeba3827549b
pldin601 <GDk4F/so>
2015-02-24 22:38:56 +0200

    inject

--- application/classes/Framework/Injector.php
+++ application/classes/Framework/Injector.php
@@ -10,19 +10,56 @@ namespace Framework;
 
 
 class Injector {
+
+    /**
+     * @param string $name
+     * @return mixed|object
+     * @throws \Exception
+     */
     public static function injectByName($name) {
-        $reflection = new \ReflectionClass($name);
-        if ($reflection->implementsInterface("Framework\\Services\\Injectable")) {
+        $class = new \ReflectionClass($name);
+        return self::injectByClass($class);
+    }
+
+    /**
+     * @param \ReflectionClass $class
+     * @return mixed|object
+     * @throws \Exception
+     */
+    public static function injectByClass($class) {
+        if ($class->implementsInterface("Framework\\Services\\Injectable")) {
             throw new \Exception("Object could not be injected");
         }
+        if ($class->implementsInterface("Tools\\SingletonInterface")) {
+            return $class->getMethod("getInstance")->invoke(null);
+        } else {
+            return $class->newInstanceArgs();
+        }
     }
-    public static function injectByClass($class) {
 
-    }
+    /**
+     * @param array $names
+     * @return array
+     * throws \Exception
+     */
     public static function injectByNameArray(array $names) {
-
+        $array = [];
+        foreach ($names as $name) {
+            $array[] = self::injectByName($name);
+        }
+        return $array;
     }
-    public static function injectByClassArray(array $classes) {
 
+    /**
+     * @param array $classes
+     * @return array
+     * throws \Exception
+     */
+    public static function injectByClassArray(array $classes) {
+        $array = [];
+        foreach ($classes as $class) {
+            $array[] = self::injectByClass($class);
+        }
+        return $array;
     }
 } 

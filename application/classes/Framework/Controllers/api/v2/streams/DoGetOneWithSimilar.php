d68ca58da7f57b781636b6c1ab0bab3531f38ef5
pldin601 <roman@homefs.biz>
2015-02-28 18:34:39 +0200

    Added comments object

--- application/classes/Framework/Controllers/api/v2/streams/DoGetOneWithSimilar.php
+++ application/classes/Framework/Controllers/api/v2/streams/DoGetOneWithSimilar.php
@@ -10,22 +10,37 @@ namespace Framework\Controllers\api\v2\streams;
 
 
 use Framework\Controller;
+use Framework\Services\DB\DBQuery;
 use Framework\Services\HttpGet;
 use Framework\Services\JsonResponse;
 use REST\Streams;
 
 class DoGetOneWithSimilar implements Controller {
 
-    public function doGet(HttpGet $get, JsonResponse $response, Streams $streams) {
+    public function doGet(HttpGet $get, JsonResponse $response, Streams $streams, DBQuery $dbq) {
 
         $id = $get->getRequired("stream_id");
 
         $one = $streams->getOneStream($id);
-        $similar = $streams->getSimilarTo($id);
+
+        if (is_array($one)) {
+            $similar = $streams->getSimilarTo($id);
+        } else {
+            $similar = [];
+        }
+
+        if (is_array($one)) {
+            $comments = $dbq->selectFrom("mor_comment")
+                ->where("comment_stream", $id)->fetchAll();
+        } else {
+            $comments = [];
+        }
+
 
         $response->setData([
             "stream"  => $one,
-            "similar" => $similar
+            "similar" => $similar,
+            "comments" => $comments
         ]);
 
     }

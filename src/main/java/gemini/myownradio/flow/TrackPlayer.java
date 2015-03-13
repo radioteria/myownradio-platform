dcc7c6d0c12ddddf495c613f96b55411d99142d1
pldin601 <GDk4F/so>
2015-03-13 23:17:52 +0200

    SharedFileAccess Implementation.

--- src/main/java/gemini/myownradio/flow/TrackPlayer.java
+++ src/main/java/gemini/myownradio/flow/TrackPlayer.java
@@ -5,7 +5,6 @@ import gemini.myownradio.exception.DecoderException;
 import gemini.myownradio.ff.FFDecoderBuilder;
 import gemini.myownradio.tools.MORLogger;
 import gemini.myownradio.tools.io.PipeIO;
-import gemini.myownradio.tools.io.SharedFile.SharedFileReader;
 
 import java.io.*;
 
@@ -70,7 +69,7 @@ public class TrackPlayer implements AbstractPlayer {
                 InputStream in = process.getInputStream();
                 OutputStream out = process.getOutputStream();
         ) {
-            PipeIO pipe = new PipeIO(new SharedFileReader(new File(file)).open(), out, true);
+            PipeIO pipe = new PipeIO(new FileInputStream(new File(file)), out, true);
             byte[] buffer = new byte[4096];
             int length, available;
             logger.println("[START]");

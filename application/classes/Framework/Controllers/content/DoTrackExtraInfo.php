0400991220a9c633eff06648a18e1cecfd3845c4
pldin601 <roman@homefs.biz>
2015-03-21 23:04:54 +0200

    Added Twig as new pattern manager

--- application/classes/Framework/Controllers/content/DoTrackExtraInfo.php
+++ application/classes/Framework/Controllers/content/DoTrackExtraInfo.php
@@ -12,14 +12,13 @@ namespace Framework\Controllers\content;
 use Framework\Controller;
 use Framework\Exceptions\ControllerException;
 use Framework\Models\AuthUserModel;
-use Framework\Services\Database;
 use Framework\Services\DB\DBQuery;
 use Framework\Services\HttpGet;
-use Framework\Template;
+use Framework\Services\TwigTemplate;
 use Objects\Track;
 
 class DoTrackExtraInfo implements Controller {
-    public function doGet(HttpGet $get, AuthUserModel $user, DBQuery $query) {
+    public function doGet(HttpGet $get, AuthUserModel $user, DBQuery $query, TwigTemplate $template) {
 
         $id = $get->getRequired("id");
 
@@ -27,37 +26,24 @@ class DoTrackExtraInfo implements Controller {
         $track = Track::getByID($id)->getOrElseNull();
 
         if ($track === null) {
-            // Track not found
+
             echo "Track not found";
+
         } else if ($track->getUserID() !== $user->getID()) {
+
             throw ControllerException::noPermission();
+
         } else {
+
             $streams = $query->selectFrom("r_streams")
                 ->innerJoin("r_link", "r_link.stream_id = r_streams.sid")
                 ->where("r_link.track_id", $id)->addGroupBy("r_streams.sid")
                 ->select("r_streams.*, COUNT(*) as times")
                 ->fetchAll("sid");
 
-            $loader = new \Twig_Loader_Filesystem("application/tmpl");
-
-            $twig = new \Twig_Environment($loader, []);
-
-            $twig->addFilter(new \Twig_SimpleFilter("json", function ($src) {
-                return json_encode($src);
-            }));
-
-            $twig->addFilter(new \Twig_SimpleFilter("ms2time", function ($src) {
-                $hours   = floor($src / 1000 / 3600);
-                $minutes = floor($src / 1000 / 60) % 60;
-                $seconds = floor($src / 1000) % 60;
-                return $hours ?
-                    sprintf("%2d:%02d:%02d", $hours, $minutes, $seconds) :
-                    sprintf("%2d:%02d", $minutes, $seconds);
-            }));
-
-            $twig->loadTemplate("track.extra.info.tmpl")->display([
-                    "track" => $track->jsonSerialize(),
-                    "appears" => $streams
+            $template->displayTemplate("track.extra.info.tmpl", [
+                "track" => $track->jsonSerialize(),
+                "appears" => $streams
             ]);
 
         }
---
+++ application/classes/Framework/Services/TwigTemplate.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LRU
+ * Date: 21.03.2015
+ * Time: 22:35
+ */
+
+namespace Framework\Services;
+
+
+use Framework\Injector\Injectable;
+use Tools\Singleton;
+use Tools\SingletonInterface;
+
+class TwigTemplate implements Injectable, SingletonInterface {
+    use Singleton;
+
+    /** @var \Twig_Environment $twig */
+    private $twig;
+
+    function __construct() {
+
+        $loader = new \Twig_Loader_Filesystem("application/tmpl");
+
+        $this->twig = new \Twig_Environment($loader, [
+            "cache" => "application/cache"
+        ]);
+
+        $this->loadFilters();
+
+    }
+
+    private function loadFilters() {
+
+        $this->twig->addFilter(new \Twig_SimpleFilter("json", function ($src) {
+            return json_encode($src);
+        }));
+
+        $this->twig->addFilter(new \Twig_SimpleFilter("ms2time", function ($src) {
+            $hours   = floor($src / 1000 / 3600);
+            $minutes = floor($src / 1000 / 60) % 60;
+            $seconds = floor($src / 1000) % 60;
+            return $hours ?
+                sprintf("%2d:%02d:%02d", $hours, $minutes, $seconds) :
+                sprintf("%2d:%02d", $minutes, $seconds);
+        }));
+
+    }
+
+    /**
+     * @param $template
+     * @param $context
+     */
+    public function displayTemplate($template, $context) {
+        $this->twig->loadTemplate($template)->display($context);
+    }
+
+    /**
+     * @param $template
+     * @param $context
+     * @return string
+     */
+    public function renderTemplate($template, $context) {
+        return $this->twig->loadTemplate($template)->render($context);
+    }
+
+}

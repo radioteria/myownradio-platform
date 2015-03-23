158bba6ec6b34efd79ab04a15d9076de9e63076f
pldin601 <GDk4F/so>
2015-03-23 21:46:13 +0200

    new: tracks sorting feature

---
+++ application/classes/Framework/Controllers/api/v2/user/DoFbLogin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 23.03.15
+ * Time: 21:32
+ */
+
+namespace Framework\Controllers\api\v2\user;
+
+
+use Framework\Controller;
+
+class DoFbLogin implements Controller {
+    public function doGet() {
+        error_log("GET");
+    }
+    public function doPost() {
+        error_log("POST");
+    }
+} 

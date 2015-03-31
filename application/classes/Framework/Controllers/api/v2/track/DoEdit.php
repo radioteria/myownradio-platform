9e9c2cf6232d81bae3a727f7332a3838cd2139dd
pldin601 <GDk4F/so>
2015-03-31 17:40:13 +0300

    Users can change permission of their tracks.

--- application/classes/Framework/Controllers/api/v2/track/DoEdit.php
+++ application/classes/Framework/Controllers/api/v2/track/DoEdit.php
@@ -34,22 +34,24 @@ class DoEdit implements Controller {
         $color      = $post->getParameter("color_id");
         $cue        = $post->getParameter("cue");
         $buy        = $post->getParameter("buy");
+        $sharable   = $post->getParameter("can_be_shared");
 
         $validator->validateTracksList($id);
 
         $this->query = $dbq->updateTable("r_tracks")
-            ->where("uid", $user->getID())
-            ->where("tid", explode(",", $id));
+                    ->where("uid", $user->getID())
+                    ->where("tid", explode(",", $id));
 
-        $artist ->then(function ($artist)   { $this->query->set("artist", $artist); });
-        $title  ->then(function ($title)    { $this->query->set("title", $title); });
-        $album  ->then(function ($album)    { $this->query->set("album", $album); });
-        $number ->then(function ($number)   { $this->query->set("track_number", $number); });
-        $genre  ->then(function ($genre)    { $this->query->set("genre", $genre); });
-        $date   ->then(function ($date)     { $this->query->set("date", $date); });
-        $color  ->then(function ($color)    { $this->query->set("color", $color); });
-        $cue    ->then(function ($cue)      { $this->query->set("cue", $cue); });
-        $buy    ->then(function ($buy)      { $this->query->set("buy", $buy); });
+        $artist     ->then(function ($artist)   { $this->query->set("artist", $artist); });
+        $title      ->then(function ($title)    { $this->query->set("title", $title); });
+        $album      ->then(function ($album)    { $this->query->set("album", $album); });
+        $number     ->then(function ($number)   { $this->query->set("track_number", $number); });
+        $genre      ->then(function ($genre)    { $this->query->set("genre", $genre); });
+        $date       ->then(function ($date)     { $this->query->set("date", $date); });
+        $color      ->then(function ($color)    { $this->query->set("color", $color); });
+        $cue        ->then(function ($cue)      { $this->query->set("cue", $cue); });
+        $buy        ->then(function ($buy)      { $this->query->set("buy", $buy); });
+        $sharable   ->then(function ($share)    { $this->query->set("can_be_shared", (bool) $share); });
 
         $this->query->update();
 
--- application/modules/audioinfo.js
+++ application/modules/audioinfo.js
@@ -36,17 +36,40 @@
 
                 var tracks = $scope.source.map(function (o) { return o.tid; }).join(",");
 
-                $scope.metadata = {
-                    title:          "",     saveTitle: true,
-                    artist:         "",     saveArtist: true,
-                    album:          "",     saveAlbum: true,
-                    trackNumber:    "",     saveTrackNumber: true,
-                    genre:          "",     saveGenre: true,
-                    date:           "",     saveDate: true,
-                    colorId:        "",     saveColorId: true,
-                    cue:            "",     saveCue: true,
-                    buy:            "",     saveBuy: true
-                };
+                var keys = [
+                    { name: "title",        default: "" },
+                    { name: "artist",       default: "" },
+                    { name: "album",        default: "" },
+                    { name: "track_number", default: "" },
+                    { name: "genre",        default: "" },
+                    { name: "date",         default: "" },
+                    { name: "color_id",     default: "" },
+                    { name: "cue",          default: "" },
+                    { name: "buy",          default: ""},
+                    { name: "can_be_shared",default: 0 }
+                ];
+
+                var metadata = {}, i, j, length;
+
+                for (i = 0; i < keys.length; i ++) {
+                    metadata[keys[i].name] = key[i].default;
+                    metadata["save_" + keys[i].name] = true;
+                }
+
+                $scope.metadata = metadata;
+
+//                $scope.metadata = {
+//                    title:          "",     saveTitle: true,
+//                    artist:         "",     saveArtist: true,
+//                    album:          "",     saveAlbum: true,
+//                    trackNumber:    "",     saveTrackNumber: true,
+//                    genre:          "",     saveGenre: true,
+//                    date:           "",     saveDate: true,
+//                    colorId:        "",     saveColorId: true,
+//                    cue:            "",     saveCue: true,
+//                    buy:            "",     saveBuy: true,
+//                    sharable:        0,    saveSharable: true
+//                };
 
                 $scope.save = function () {
 
@@ -54,101 +77,130 @@
                         track_id: tracks
                     };
 
-                    if ($scope.metadata.saveTitle)
-                        data.title = $scope.metadata.title;
-                    if ($scope.metadata.saveArtist)
-                        data.artist = $scope.metadata.artist;
-                    if ($scope.metadata.saveAlbum)
-                        data.album = $scope.metadata.album;
-                    if ($scope.metadata.saveTrackNumber)
-                        data.track_number = $scope.metadata.trackNumber;
-                    if ($scope.metadata.saveGenre)
-                        data.genre = $scope.metadata.genre;
-                    if ($scope.metadata.saveDate)
-                        data.date = $scope.metadata.date;
-                    if ($scope.metadata.saveColorId)
-                        data.color_id = $scope.metadata.colorId;
-                    if ($scope.metadata.saveCue)
-                        data.cue = $scope.metadata.cue;
-                    if ($scope.metadata.saveBuy)
-                        data.buy = $scope.metadata.buy;
+                    for (i = 0; i < keys.length; i ++) {
+                        if ($scope.metadata["save_" + keys[i].name]) {
+                            data[keys[i].name] = $scope.metadata[keys[i].name];
+                        }
+                    }
+
+//                    if ($scope.metadata.saveTitle)
+//                        data.title = $scope.metadata.title;
+//                    if ($scope.metadata.saveArtist)
+//                        data.artist = $scope.metadata.artist;
+//                    if ($scope.metadata.saveAlbum)
+//                        data.album = $scope.metadata.album;
+//                    if ($scope.metadata.saveTrackNumber)
+//                        data.track_number = $scope.metadata.trackNumber;
+//                    if ($scope.metadata.saveGenre)
+//                        data.genre = $scope.metadata.genre;
+//                    if ($scope.metadata.saveDate)
+//                        data.date = $scope.metadata.date;
+//                    if ($scope.metadata.saveColorId)
+//                        data.color_id = $scope.metadata.colorId;
+//                    if ($scope.metadata.saveCue)
+//                        data.cue = $scope.metadata.cue;
+//                    if ($scope.metadata.saveBuy)
+//                        data.buy = $scope.metadata.buy;
 
                     AudioInfoEditor.save(data).onSuccess(function () {
-                        for (var i = 0, length = $scope.source.length; i < length; i += 1) {
-                            if ($scope.metadata.saveTitle)
-                                $scope.source[i].title = $scope.metadata.title;
-                            if ($scope.metadata.saveArtist)
-                                $scope.source[i].artist = $scope.metadata.artist;
-                            if ($scope.metadata.saveAlbum)
-                                $scope.source[i].album = $scope.metadata.album;
-                            if ($scope.metadata.saveTrackNumber)
-                                $scope.source[i].track_number = $scope.metadata.trackNumber;
-                            if ($scope.metadata.saveGenre)
-                                $scope.source[i].genre = $scope.metadata.genre;
-                            if ($scope.metadata.saveDate)
-                                $scope.source[i].date = $scope.metadata.date;
-                            if ($scope.metadata.saveColorId)
-                                $scope.source[i].color_id = $scope.metadata.colorId;
-                            if ($scope.metadata.saveCue)
-                                $scope.source[i].cue = $scope.metadata.cue;
-                            if ($scope.metadata.saveBuy)
-                                $scope.source[i].buy = $scope.metadata.buy;
+                        for (i = 0, length = $scope.source.length; i < length; i += 1) {
+
+                            for (j = 0; j < keys.length; j ++) {
+                                if ($scope.metadata["save_" + keys[j].name]) {
+                                    data[keys[j].name] = metadata[keys[j].name];
+                                    $scope.source[i][keys[j].name] = $scope.metadata[keys[j].name];
+                                }
+                            }
+
+//                            if ($scope.metadata.saveTitle)
+//                                $scope.source[i].title = $scope.metadata.title;
+//                            if ($scope.metadata.saveArtist)
+//                                $scope.source[i].artist = $scope.metadata.artist;
+//                            if ($scope.metadata.saveAlbum)
+//                                $scope.source[i].album = $scope.metadata.album;
+//                            if ($scope.metadata.saveTrackNumber)
+//                                $scope.source[i].track_number = $scope.metadata.trackNumber;
+//                            if ($scope.metadata.saveGenre)
+//                                $scope.source[i].genre = $scope.metadata.genre;
+//                            if ($scope.metadata.saveDate)
+//                                $scope.source[i].date = $scope.metadata.date;
+//                            if ($scope.metadata.saveColorId)
+//                                $scope.source[i].color_id = $scope.metadata.colorId;
+//                            if ($scope.metadata.saveCue)
+//                                $scope.source[i].cue = $scope.metadata.cue;
+//                            if ($scope.metadata.saveBuy)
+//                                $scope.source[i].buy = $scope.metadata.buy;
                         }
                         $scope.closeThisDialog();
                     }, function (error) {
-                        $dialog.info(error);
+                        $dialog.message(error);
                     });
 
                 };
 
-                for (var i = 0, length = $scope.source.length; i < length; i += 1) {
+                for (i = 0, length = $scope.source.length; i < length; i += 1) {
                     if (i == 0) {
-                        $scope.metadata.title       = $scope.source[i].title;
-                        $scope.metadata.artist      = $scope.source[i].artist;
-                        $scope.metadata.album       = $scope.source[i].album;
-                        $scope.metadata.trackNumber = $scope.source[i].track_number;
-                        $scope.metadata.genre       = $scope.source[i].genre;
-                        $scope.metadata.date        = $scope.source[i].date;
-                        $scope.metadata.colorId     = $scope.source[i].color;
-                        $scope.metadata.cue         = $scope.source[i].cue;
-                        $scope.metadata.buy         = $scope.source[i].buy;
+
+                        for (j = 0; j < keys.length; j ++) {
+                            $scope.metadata[keys[j].name]       = $scope.source[i][keys[j].name];
+                        }
+
+//
+//                        $scope.metadata.artist      = $scope.source[i].artist;
+//                        $scope.metadata.album       = $scope.source[i].album;
+//                        $scope.metadata.trackNumber = $scope.source[i].track_number;
+//                        $scope.metadata.genre       = $scope.source[i].genre;
+//                        $scope.metadata.date        = $scope.source[i].date;
+//                        $scope.metadata.colorId     = $scope.source[i].color;
+//                        $scope.metadata.cue         = $scope.source[i].cue;
+//                        $scope.metadata.buy         = $scope.source[i].buy;
                     } else {
-                        if ($scope.metadata.title != $scope.source[i].title) {
-                            $scope.metadata.saveTitle = false;
-                            $scope.metadata.title = ""
+
+                        for (j = 0; j < keys.length; j ++) {
+
+                            if ($scope.metadata[keys[j].name] != $scope.source[i][keys[j].name]) {
+                                $scope.metadata["save_" + keys[j].name] = false;
+                                $scope.metadata[keys[j]] = keys[j].default;
+                            }
+
                         }
-                        if ($scope.metadata.artist != $scope.source[i].artist) {
-                            $scope.metadata.saveArtist = false;
-                            $scope.metadata.artist = ""
-                        }
-                        if ($scope.metadata.album != $scope.source[i].album) {
-                            $scope.metadata.saveAlbum = false;
-                            $scope.metadata.album = ""
-                        }
-                        if ($scope.metadata.trackNumber != $scope.source[i].track_number) {
-                            $scope.metadata.saveTrackNumber = false;
-                            $scope.metadata.trackNumber = ""
-                        }
-                        if ($scope.metadata.genre != $scope.source[i].genre) {
-                            $scope.metadata.saveGenre = false;
-                            $scope.metadata.genre = ""
-                        }
-                        if ($scope.metadata.date != $scope.source[i].date) {
-                            $scope.metadata.saveDate = false;
-                            $scope.metadata.date = ""
-                        }
-                        if ($scope.metadata.colorId != $scope.source[i].color) {
-                            $scope.metadata.saveColorId = false;
-                            $scope.metadata.colorId = ""
-                        }
-                        if ($scope.metadata.cue != $scope.source[i].cue) {
-                            $scope.metadata.saveCue = false;
-                            $scope.metadata.cue = ""
-                        }
-                        if ($scope.metadata.buy != $scope.source[i].buy) {
-                            $scope.metadata.saveBuy = false;
-                            $scope.metadata.buy = ""
-                        }
+
+//                        if ($scope.metadata.title != $scope.source[i].title) {
+//                            $scope.metadata.saveTitle = false;
+//                            $scope.metadata.title = ""
+//                        }
+//                        if ($scope.metadata.artist != $scope.source[i].artist) {
+//                            $scope.metadata.saveArtist = false;
+//                            $scope.metadata.artist = ""
+//                        }
+//                        if ($scope.metadata.album != $scope.source[i].album) {
+//                            $scope.metadata.saveAlbum = false;
+//                            $scope.metadata.album = ""
+//                        }
+//                        if ($scope.metadata.trackNumber != $scope.source[i].track_number) {
+//                            $scope.metadata.saveTrackNumber = false;
+//                            $scope.metadata.trackNumber = ""
+//                        }
+//                        if ($scope.metadata.genre != $scope.source[i].genre) {
+//                            $scope.metadata.saveGenre = false;
+//                            $scope.metadata.genre = ""
+//                        }
+//                        if ($scope.metadata.date != $scope.source[i].date) {
+//                            $scope.metadata.saveDate = false;
+//                            $scope.metadata.date = ""
+//                        }
+//                        if ($scope.metadata.colorId != $scope.source[i].color) {
+//                            $scope.metadata.saveColorId = false;
+//                            $scope.metadata.colorId = ""
+//                        }
+//                        if ($scope.metadata.cue != $scope.source[i].cue) {
+//                            $scope.metadata.saveCue = false;
+//                            $scope.metadata.cue = ""
+//                        }
+//                        if ($scope.metadata.buy != $scope.source[i].buy) {
+//                            $scope.metadata.saveBuy = false;
+//                            $scope.metadata.buy = ""
+//                        }
                     }
                 }
 

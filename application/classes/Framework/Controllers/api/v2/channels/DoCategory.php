86b147244793be44739b80f2c0ebc8763029a3fe
pldin601 <GDk4F/so>
2015-04-05 14:48:20 +0300

    New API branch.

--- application/classes/Framework/Controllers/api/v2/channels/DoCategory.php
+++ application/classes/Framework/Controllers/api/v2/channels/DoCategory.php
@@ -13,10 +13,10 @@ use API\ChannelsCollection;
 use Framework\Controller;
 use Framework\Services\HttpGet;
 use Framework\Services\JsonResponse;
-use Framework\Services\Validator;
+use Framework\Services\Validators\CategoryValidator;
 
 class DoCategory implements Controller {
-    public function doGet(HttpGet $get, ChannelsCollection $collection, Validator $validator, JsonResponse $response) {
+    public function doGet(HttpGet $get, ChannelsCollection $collection, CategoryValidator $validator, JsonResponse $response) {
         $category_name = $get->getRequired("category_name");
         $offset = $get->getParameter("offset", FILTER_VALIDATE_INT)->getOrElse(0);
         $limit = $get->getParameter("limit", FILTER_VALIDATE_INT)->getOrElseNull();

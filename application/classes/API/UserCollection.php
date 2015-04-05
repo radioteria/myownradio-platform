9187654dae73df7fca3aa8576fce366f8cd9f006
pldin601 <GDk4F/so>
2015-04-05 15:30:24 +0300

    New API branch.

---
+++ application/classes/API/UserCollection.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 05.04.15
+ * Time: 15:28
+ */
+
+namespace API;
+
+
+use Framework\Injector\Injectable;
+use Framework\Services\DB\Query\SelectQuery;
+use Tools\Singleton;
+use Tools\SingletonInterface;
+
+class UserCollection implements Injectable, SingletonInterface  {
+    use Singleton;
+
+    private function getUsersPrefix() {
+
+        $prefix = (new SelectQuery("mor_users_view"))
+            ->select("uid", "name", "permalink", "avatar", "streams_count",
+                "tracks_count", "info", "plan_id", "country_id");
+
+        return $prefix;
+
+    }
+} 

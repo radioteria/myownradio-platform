d21a153c2802e775ca3af27a28b037ff9cdb3537
pldin601 <roman@homefs.biz>
2015-04-06 16:40:02 +0300

    View by radio station tag.

--- application/modules/filters.js
+++ application/modules/filters.js
@@ -8,7 +8,7 @@
 
     app.filter("usersCatalog", ['ROUTES', function (ROUTES) {
         return function (key) {
-            return ROUTES.PATH_USERS_CATALOG[0] + "/" + key;
+            return ROUTES.PATH_USERS_CATALOG[0] + key;
         }
     }])
 

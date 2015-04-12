7d79015a6beb6e2baf67157235be14ea05261d8d
Roman Gemini <roman@homefs.biz>
2015-04-12 21:47:42 +0300

    Upload progress bar.

--- application/classes/Framework/Handlers/DoGradient.php
+++ application/classes/Framework/Handlers/DoGradient.php
@@ -11,21 +11,28 @@ namespace Framework\Handlers;
 
 use Framework\Controller;
 use Framework\FileServer\FSFile;
+use Framework\Services\DB\Query\SelectQuery;
 use Objects\FileServer\FileServerFile;
 
 class DoGradient implements Controller {
     public function doGet() {
 
-        header("Content-Type: text/plain");
-        set_time_limit(30);
-
-        $files = FileServerFile::getListByFilter("use_count = 0");
-
-        foreach ($files as $file) {
-            FSFile::deleteLink($file->getFileId());
-        }
-
-        echo count($files);
+//        $legacy = "./legacy";
+//
+//        $modules = (new SelectQuery("r_modules"))->fetchAll();
+//        echo getcwd();
+//        $keys = ["css", "js", "html", "tmpl", "post"];
+//        foreach ($modules as $module) {
+//
+//            foreach ($keys as $key) {
+//
+//                if (strlen($module[$key]) > 0) {
+//
+//                    mkdir($legacy."/".$key, 0777, true);
+//                    file_put_contents($legacy."/".$key."/".$module["name"].".".$key, $module[$key]);
+//                }
+//            }
+//        }
 
     }
 

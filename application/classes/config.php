d3eb19308186ca9ab532728c3c81c19ecfe98615
Roman Gemini <roman@homefs.biz>
2015-04-13 14:18:41 +0300

    DBQuery duplicate key update.

--- application/classes/config.php
+++ application/classes/config.php
@@ -6,5 +6,16 @@
  * Time: 13:57
  */
 return [
-    "APPLICATION_TITLE" => "MyOwnRadio - your own free web radio station"
+    "application_title" => "MyOwnRadio - your own free web radio station",
+    "api" => [
+        "streams_per_request_max" => 50,
+        "tracks_per_request_max" => 50
+    ],
+    "media" => [
+        "track_extension_pattern" => "mp3|flac|ogg|m4a|ape|aac",
+        "track_duration_min" => 15000,
+        "track_duration_max" => 14400000,
+        "track_size_max" => 536870912,
+        "track_preview_command" => "ffmpeg -v quiet -ss %time% -i %path% -filter 'afade=t=in:ss=0:d=2' -vn -acodec libmp3lame -ac 2 -ar 44100 -ab 128k -f mp3 -"
+    ]
 ];

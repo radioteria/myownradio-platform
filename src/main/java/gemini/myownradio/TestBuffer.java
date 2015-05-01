c502b3b3df183584ce7a53b0c83f063d2b6a6d72
Roman Gemini <roman@homefs.biz>
2015-05-01 23:07:23 +0300

    Added Notif1er.

--- src/main/java/gemini/myownradio/TestBuffer.java
+++ src/main/java/gemini/myownradio/TestBuffer.java
@@ -1,5 +1,6 @@
 package gemini.myownradio;
 
+import gemini.myownradio.engine.Notif1er;
 import gemini.myownradio.tools.io.SharedFile.SharedFileReader;
 
 import java.io.File;
@@ -12,31 +13,7 @@ import java.io.InputStream;
 public class TestBuffer {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        int threadCount = 4;
-        File file = new File("/Volumes/3TB/Фильмы/Jacques.Cousteau.The.Silent.World.1956.720p.BluRay.Rus.Fre.Eng.HDCLUB.mkv");
+        new Notif1er.Event("Hello", 1, 2).send("test");
 
-        Runnable r = () -> {
-            try (InputStream is = new SharedFileReader(file).open()) {
-                int length;
-                byte[] buffer = new byte[4096];
-                long count = 0;
-                while ((length = is.read(buffer)) != 0) {
-                    count += length;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        };
-
-        Thread[] threads = new Thread[threadCount];
-
-        for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(r);
-            threads[i].start();
-        }
-
-        for (Thread t : threads) {
-            t.join();
-        }
     }
 }

6a311a2631777db1f85260c9b628473a0f5d1e75
pldin601 <roman@homefs.biz>
2015-05-13 13:16:55 +0300

    LiqPay added.

--- application/classes/Framework/Handlers/api/v3/DoPayment.php
+++ application/classes/Framework/Handlers/api/v3/DoPayment.php
@@ -10,7 +10,6 @@ namespace Framework\Handlers\api\v3;
 
 
 use Framework\ControllerImpl;
-use Framework\Exceptions\ControllerException;
 use Framework\Models\PaymentModel;
 use Framework\Services\HttpPost;
 use Framework\View\Errors\View400Exception;
@@ -19,12 +18,14 @@ use LiqPay;
 class DoPayment extends ControllerImpl {
     public function doPost(HttpPost $post) {
 
-        $data = $post->getParameter("data")->getOrElseThrow(View400Exception::getClass());
-        $signature = $post->getRequired("signature");
+        $data = $post->getParameter("data")
+            ->getOrElseThrow(View400Exception::getClass());
+        $signature = $post->getParameter("signature")
+            ->getOrElseThrow(View400Exception::getClass());
 
         // Check signature
         if (base64_encode(sha1(LiqPay::$private_key . $data . LiqPay::$private_key, 1)) !== $signature) {
-            throw new ControllerException("Wrong signature!");
+            throw new View400Exception("Wrong signature!");
         }
 
         $json = base64_decode($data);

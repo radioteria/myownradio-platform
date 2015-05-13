d527803d7b7d48f94552e3a392e35c18c5632182
pldin601 <roman@homefs.biz>
2015-05-13 12:36:15 +0300

    LiqPay added.

--- application/classes/Framework/Handlers/api/v3/DoAcquire.php
+++ application/classes/Framework/Handlers/api/v3/DoAcquire.php
@@ -20,6 +20,12 @@ class DoAcquire extends ControllerImpl {
         $plan_id = $get->getParameter("plan_id")
             ->getOrElseThrow(new View500Exception("plan_id not specified"));
 
+
+        if ($plan_id < 2 && $plan_id > 5) {
+            throw new View500Exception("Wrong plan_id");
+        }
+
+
         $html = PaymentModel::createOrder($plan_id);
 
         http_response_code(302);

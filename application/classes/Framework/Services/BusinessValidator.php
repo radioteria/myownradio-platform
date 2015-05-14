986fc0a749af1052696e06bd7827264747086a73
pldin601 <roman@homefs.biz>
2015-05-14 15:52:22 +0300

    Added new Business Validator.

---
+++ application/classes/Framework/Services/BusinessValidator.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roman
+ * Date: 14.05.15
+ * Time: 15:37
+ */
+
+namespace Framework\Services;
+
+
+use Framework\Services\DB\DBQuery;
+
+class BusinessValidator extends Validator {
+
+    const PERMALINK_REGEXP_PATTERN = "~(^[a-z0-9\\-]*$)~";
+    const LOGIN_PATTERN = "~^[0-9a-z\\_]+$~";
+
+    function login() {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) { return preg_match(self::LOGIN_PATTERN, $value); });
+        return $copy;
+    }
+
+    function permalink() {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) { return is_null($value) || preg_match(self::PERMALINK_REGEXP_PATTERN, $value); });
+        return $copy;
+    }
+
+    function checkPermalinkForUser($user_id) {
+
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) use ($user_id) {
+
+            $dbq = DBQuery::getInstance();
+
+            $query = $dbq->selectFrom("r_users")
+                ->where("(permalink = :key OR uid = :key) AND (uid != :user)", [
+                    ":key" => $value,
+                    ":user" => $user_id
+                ]);
+
+            return count($query) == 0;
+
+        });
+        return $copy;
+
+    }
+
+    function checkPermalinkForStream($stream_id) {
+
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) use ($stream_id) {
+
+            $dbq = DBQuery::getInstance();
+
+            $query = $dbq->selectFrom("r_streams")
+                ->where("(permalink = :key OR sid = :key) AND (sid != :stream)", [
+                    ":key" => $value,
+                    ":stream" => $stream_id
+                ]);
+
+            return count($query) == 0;
+
+        });
+
+        return $copy;
+
+    }
+
+} 
--- application/classes/Framework/Services/Validator.php
+++ application/classes/Framework/Services/Validator.php
@@ -10,9 +10,112 @@ namespace Framework\Services;
 
 
 use Framework\Injector\Injectable;
+use Tools\Optional;
 use Tools\Singleton;
 use Tools\SingletonInterface;
 
-class Validator implements Injectable, SingletonInterface {
-    use Singleton;
+class Validator implements Injectable {
+
+    const EMAIL_REGEXP_PATTERN = "~^[\\w\\S]+@[\\w\\S]+\\.[\\w]{2,4}$~";
+
+    /** @var callable[] */
+    protected $predicates = [];
+
+    protected $variable = null;
+
+    function __construct($variable, $predicates = []) {
+        $this->variable = $variable;
+        $this->predicates = $predicates;
+    }
+
+    function number() {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) { return is_numeric($value); });
+        return $copy;
+    }
+
+    function string() {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) { return is_string($value); });
+        return $copy;
+    }
+
+    function min_length($length) {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) use ($length) { return strlen($value) >= $length; });
+        return $copy;
+    }
+
+    function max_length($length) {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) use ($length) { return strlen($value) <= $length; });
+        return $copy;
+    }
+
+    function greaterOrEqual($than) {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) use ($than) { return $value >= $than; });
+        return $copy;
+    }
+
+    function lessOrEqual($than) {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) use ($than) { return $value <= $than; });
+        return $copy;
+    }
+
+    function pattern($pattern) {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) use ($pattern) { return preg_match($pattern, $value); });
+        return $copy;
+    }
+
+    function email() {
+        $copy = $this->copy();
+        $copy->addPredicate(function ($value) { return preg_match(self::EMAIL_REGEXP_PATTERN, $value); });
+        return $copy;
+    }
+
+
+    /**
+     * @return Validator
+     */
+    protected function copy() {
+        return new Validator($this->variable, $this->predicates);
+    }
+
+    /**
+     * @param callable $callable
+     */
+    protected function addPredicate(callable $callable) {
+        $this->predicates[] = $callable;
+    }
+
+    /**
+     * @param \Exception $exception
+     */
+    public function throwOnFail(\Exception $exception) {
+        $this->run()->justThrow($exception);
+    }
+
+    /**
+     * @param callable $callable
+     */
+    public function doOnSuccess(Callable $callable) {
+        $this->run()->then($callable);
+    }
+
+    /**
+     * @return Optional
+     */
+    public function run() {
+        foreach ($this->predicates as $predicate) {
+            $result = call_user_func_array($predicate, $this->variable);
+            if (!$result) {
+                return Optional::noValue();
+            }
+        }
+        return Optional::hasValue($this->variable);
+    }
+
 }

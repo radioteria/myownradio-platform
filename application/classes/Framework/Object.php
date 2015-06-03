bb3988f723e6877ba787e43cfe1c024027403418
pldin601 <roman@homefs.biz>
2015-06-03 14:07:05 +0300

    Re-Internationalization.

--- application/classes/Framework/Object.php
+++ application/classes/Framework/Object.php
@@ -9,11 +9,20 @@
 namespace Framework;
 
 
+use ReflectionClass;
+
 trait Object {
     /**
      * @return string
      */
     public static function className() {
         return get_called_class();
+    }
+
+    /**
+     * @return ReflectionClass
+     */
+    public static function getClass() {
+        return new ReflectionClass(self::className());
     }
 } 

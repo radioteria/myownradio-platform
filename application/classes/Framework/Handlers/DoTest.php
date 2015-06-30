565ec2156005ab5f598a1421ef85b4952617571c
pldin601 <roman@homefs.biz>
2015-06-30 12:43:45 +0300

    Monad testing.

--- application/classes/Framework/Handlers/DoTest.php
+++ application/classes/Framework/Handlers/DoTest.php
@@ -10,23 +10,26 @@ namespace Framework\Handlers;
 
 
 use Framework\ControllerImpl;
+use Framework\Exceptions\Entity\UserNotFoundException;
 use Objects\User;
 use Tools\Optional\Filter;
 use Tools\Optional\Option;
 use Tools\Optional\Transform;
 
 class DoTest extends ControllerImpl {
+
     public function doGet(Option $id) {
 
-        $userName = $id->map(Transform::toNumber())
+        $userName = $id->filter(Filter::isNumber())
+            ->map(Transform::toNumber())
             ->reject(Filter::value(1))
             ->flatMap(Transform::call(User::class, "getById"))
-            ->map(Transform::method("getName"));
+            ->map(Transform::method("getName"))
+            ->orThrow(UserNotFoundException::class);
 
-
-        echo $userName;
-
+        echo "Hello, {$userName}!";
 
     }
+
 }
 

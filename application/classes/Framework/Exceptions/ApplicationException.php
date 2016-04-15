71774e0096ae2955436cbad40eaa35a2fc73ae64
tr808 <tr808@homefs.biz>
2016-04-15 16:08:53 +0300

    Relocation fix

--- application/classes/Framework/Exceptions/ApplicationException.php
+++ application/classes/Framework/Exceptions/ApplicationException.php
@@ -13,11 +13,12 @@ use Exception;
 use Framework\View\Errors\View500Exception;
 
 class ApplicationException extends Exception {
-    public function __construct($message = "", $code = 0, Exception $previous = null) {
+    public function __construct($message = null, $code = null, Exception $previous = null) {
+	error_log($message);
         parent::__construct($message, $code, $previous);
     }
 
-    public static function of($message = "", $code = 0, Exception $previous = null) {
+    public static function of($message = null, $code = null, Exception $previous = null) {
         return new self($message, $code, $previous);
     }
 

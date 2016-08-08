c32ab7cdfa76aac56ef66fdd244a74a32e270f07
Roman Gemini <roman@homefs.biz>
2016-08-08 19:28:23 +0300

    added hibernate

---
+++ src/main/java/biz/streamserver/dao/AbstractDao.java
@@ -0,0 +1,14 @@
+package biz.streamserver.dao;
+
+import javax.annotation.Resource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+/**
+ * Created by roman on 8/8/16
+ */
+public abstract class AbstractDao<T> {
+
+}

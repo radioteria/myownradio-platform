e199689cf28b6507ac9b3ec5e60ef2cc038b44d8
Roman Gemini <roman@homefs.biz>
2016-11-11 12:46:56 +0200

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,19 @@ const minify = require('gulp-minify');
 
 gulp.task('scripts', function () {
     gulp.src([
-        'bower_components/jquery/dist/jquery.js',
-        'bower_components/jquery-migrate/jquery-migrate.js',
-        'bower_components/jquery-ui/jquery-ui.js',
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/jquery-migrate/jquery-migrate.min.js',
+        'bower_components/jquery-ui/jquery-ui.min.js',
         'bower_components/jquery.cookie/jquery.cookie.js',
-        'bower_components/jquery-livequery/dist/jquery.livequery.js',
-        'bower_components/jPlayer/dist/jplayer/jquery.jplayer.js',
+        'bower_components/jquery-livequery/dist/jquery.livequery.min.js',
+        'bower_components/jPlayer/dist/jplayer/jquery.jplayer.min.js',
 
-        'bower_components/angular/angular.js',
-        'bower_components/angular-route/angular-route.js',
-        'bower_components/angular-animate/angular-animate.js',
+        'bower_components/angular/angular.min.js',
+        'bower_components/angular-route/angular-route.min.js',
+        'bower_components/angular-animate/angular-animate.min.js',
         'bower_components/angular-contenteditable/angular-contenteditable.js',
-        'bower_components/mixpanel/mixpanel-jslib-snippet.js',
-        'bower_components/angular-mixpanel/src/angular-mixpanel.js',
+        'bower_components/mixpanel/mixpanel-jslib-snippet.min.js',
+        'bower_components/angular-mixpanel/dist/angular-mixpanel.min.js',
     ])
         .pipe(minify())
         .pipe(concat('dist.min.js'))

2251df97c9e94a29115189fefbc395b47a98afac
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-06-18 13:55:52 +0300

    Remove commented code

--- application/classes/Framework/Models/AuthUserModel.php
+++ application/classes/Framework/Models/AuthUserModel.php
@@ -32,12 +32,6 @@ class AuthUserModel extends UserModel implements Injectable {
 
         $exception = UnauthorizedException::unAuthorized();
 
-//        $token = HttpSession::getInstance()->get("TOKEN")->getOrElse(
-//            HttpPost::getInstance()->getParameter("token")->getOrElse(
-//                HttpGet::getInstance()->getParameter("token")->getOrElseThrow($exception)
-//            )
-//        );
-
         $token = HttpSession::getInstance()->get("TOKEN")->getOrElseThrow($exception);
 
         $session = Database::doInConnection(function (Database $db) use ($token, $exception) {

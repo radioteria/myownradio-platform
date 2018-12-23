91bc8283188bf2944100cc55920c180ef08f1319
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2018-12-23 16:25:10 +0200

    Initial environment refactoring

--- services/radionode/src/server.ts
+++ services/radionode/src/server.ts
@@ -1,6 +1,6 @@
-import { config } from 'dotenv';
-import app from './app';
+import { config } from "dotenv";
+import app from "./app";
 
 config();
 
-app(process.env.PORT, process.env.BACKEND);
+app(8080, "mor");

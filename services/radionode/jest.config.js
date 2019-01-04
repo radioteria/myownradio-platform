5d40207ea34fecad64aa758b1d6191aeb876102c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-01-04 22:43:43 +0200

    Remove coverage reporting

--- services/radionode/jest.config.js
+++ services/radionode/jest.config.js
@@ -6,6 +6,4 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   roots: ['src'],
   testPathIgnorePatterns: ['/node_modules/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**'],
 };

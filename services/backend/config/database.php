19e61f65c14d1964c7dfeac4a32aceb9fecb34da
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2021-03-07 18:21:30 +0200

    chore: disable persistent database connection

--- services/backend/config/database.php
+++ services/backend/config/database.php
@@ -13,7 +13,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'attributes' => [
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::ATTR_PERSISTENT => true,
+//                PDO::ATTR_PERSISTENT => true,
                 PDO::ATTR_AUTOCOMMIT => true,
                 PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION

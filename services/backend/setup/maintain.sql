f83e75c1b287709684cf4330171d122f0d6e9afe
Roma Lakhtadyr <roman.lakhtadyr@gmail.com>
2021-03-28 21:41:31 +0300

    feat: create database for local development

--- services/backend/setup/maintain.sql
+++
@@ -1,8 +0,0 @@
---
--- Fix user static counters
---
-
-UPDATE r_static_user_vars SET
-  tracks_count = IFNULL((SELECT COUNT(*) FROM r_tracks WHERE uid = r_static_user_vars.user_id), 0),
-  tracks_duration = IFNULL((SELECT SUM(r_tracks.duration) FROM r_tracks WHERE uid = r_static_user_vars.user_id), 0),
-  tracks_size = IFNULL((SELECT SUM(r_tracks.filesize) FROM r_tracks WHERE uid = r_static_user_vars.user_id), 0)

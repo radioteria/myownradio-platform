aa1dba24db78de6a3ccc076845c2a5d1518976fd
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2021-04-04 21:08:42 +0300

    feat: add extensions to file objects and add modplug modules playback support

--- services/backend/application/classes/Objects/FileServer/FileServerFile.php
+++ services/backend/application/classes/Objects/FileServer/FileServerFile.php
@@ -7,6 +7,7 @@
  */
 
 namespace Objects\FileServer;
+
 use Framework\Services\ORM\EntityUtils\ActiveRecord;
 use Framework\Services\ORM\EntityUtils\ActiveRecordObject;
 
@@ -18,82 +19,108 @@ use Framework\Services\ORM\EntityUtils\ActiveRecordObject;
  * @do_HASH file_hash = ?
  * @do_UNUSED use_count = 0
  */
-class FileServerFile extends ActiveRecordObject implements ActiveRecord {
+class FileServerFile extends ActiveRecordObject implements ActiveRecord
+{
     private $file_id;
     private $file_size;
     private $file_hash;
+    private string $file_extension;
     private $server_id;
     private $use_count;
 
     /**
      * @return mixed
      */
-    public function getFileHash() {
+    public function getFileHash()
+    {
         return $this->file_hash;
+    }
+
+    /**
+     * @param mixed $file_hash
+     */
+    public function setFileHash($file_hash)
+    {
+        $this->file_hash = $file_hash;
     }
 
     /**
      * @return mixed
      */
-    public function getFileId() {
+    public function getFileId()
+    {
         return $this->file_id;
     }
 
     /**
      * @return mixed
      */
-    public function getFileSize() {
+    public function getFileSize()
+    {
         return $this->file_size;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getServerId() {
-        return $this->server_id;
-    }
-
-    /**
-     * @return FileServer|null
-     */
-    public function getServerObject() {
-        return FileServer::getByID($this->server_id)->getOrElseNull();
-    }
-
-    /**
-     * @param mixed $file_hash
-     */
-    public function setFileHash($file_hash) {
-        $this->file_hash = $file_hash;
     }
 
     /**
      * @param mixed $file_size
      */
-    public function setFileSize($file_size) {
+    public function setFileSize($file_size)
+    {
         $this->file_size = $file_size;
-    }
-
-    /**
-     * @param mixed $server_id
-     */
-    public function setServerId($server_id) {
-        $this->server_id = $server_id;
-    }
-
-    /**
-     * @param mixed $use_count
-     */
-    public function setUseCount($use_count) {
-        $this->use_count = $use_count;
     }
 
     /**
      * @return mixed
      */
-    public function getUseCount() {
+    public function getServerId()
+    {
+        return $this->server_id;
+    }
+
+    /**
+     * @param mixed $server_id
+     */
+    public function setServerId($server_id)
+    {
+        $this->server_id = $server_id;
+    }
+
+    /**
+     * @return FileServer|null
+     */
+    public function getServerObject()
+    {
+        return FileServer::getByID($this->server_id)->getOrElseNull();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUseCount()
+    {
         return $this->use_count;
     }
 
+    /**
+     * @param mixed $use_count
+     */
+    public function setUseCount($use_count)
+    {
+        $this->use_count = $use_count;
+    }
 
-} 
+    /**
+     * @return string
+     */
+    public function getFileExtension(): string
+    {
+        return $this->file_extension;
+    }
+
+    /**
+     * @param string $file_extension
+     */
+    public function setFileExtension(string $file_extension): void
+    {
+        $this->file_extension = $file_extension;
+    }
+}

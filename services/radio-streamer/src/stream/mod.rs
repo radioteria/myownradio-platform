2c4241dbe6fcddd3634bf6534e1f5e77ed8c53b7
Roman L <roman.lakhtadyr@gmail.com>
2021-12-03 22:24:13 +0200

    feat: create ffmpeg encoder

--- services/radio-streamer/src/stream/mod.rs
+++ services/radio-streamer/src/stream/mod.rs
@@ -2,6 +2,7 @@ mod constants;
 
 pub(crate) mod channel_player;
 pub(crate) mod ffmpeg_decoder;
+pub(crate) mod ffmpeg_encoder;
 pub(crate) mod player_loop;
 pub(crate) mod player_registry;
 pub(crate) mod types;

ff0bbcaf8a8e2ee3b1b656690808b3c72150e32b
Roman L <roman.lakhtadyr@gmail.com>
2021-12-25 20:19:25 +0200

    feat: add turborepo

--- services/ui/sdk/src/index.ts
+++
@@ -1,7 +0,0 @@
-export interface UIModule {
-  // What should be done after mounting.
-  mount(element: HTMLElement): Promise<void>
-
-  // What should be done after unmounting.
-  unmount(element: HTMLElement): Promise<void>
-}

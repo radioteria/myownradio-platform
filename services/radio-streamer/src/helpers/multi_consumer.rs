c80d645610e81f0073d221cf62715fe0b020e0dc
Roman L <roman.lakhtadyr@gmail.com>
2022-10-03 23:02:01 +0100

    feat(radio-streamer): auto-close MultiConsumer if no consumers

--- services/radio-streamer/src/helpers/multi_consumer.rs
+++ services/radio-streamer/src/helpers/multi_consumer.rs
@@ -1,6 +1,8 @@
+use actix_rt::task::JoinHandle;
 use futures::channel::mpsc;
 use futures::StreamExt;
 use std::collections::HashSet;
+use std::future::Future;
 use std::mem;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
@@ -23,9 +25,14 @@ impl<T: Clone + 'static> Inner<T> {
         let child_sender_receive_timeout = Duration::from_secs(60);
         let child_sender_buffer_size = 10;
 
-        let children_senders = Arc::new(Mutex::new(Vec::<mpsc::Sender<T>>::new()));
+        let children_senders: Arc<Mutex<Vec<mpsc::Sender<T>>>> = Arc::default();
 
-        actix_rt::spawn({
+        let mut no_senders_timeout_handle_container = None::<JoinHandle<()>>;
+        let loop_handle_container = Arc::new(Mutex::new(None::<JoinHandle<()>>));
+
+        let loop_handle = actix_rt::spawn({
+            let loop_handle_container = loop_handle_container.clone();
+
             let children_senders = children_senders.clone();
             let mut source_receiver = source_receiver;
 
@@ -35,9 +42,33 @@ impl<T: Clone + 'static> Inner<T> {
                         .lock()
                         .expect("Unable to lock children_senders on sending data");
 
+                    if locked_senders.is_empty() {
+                        let loop_handle_container = loop_handle_container.clone();
+
+                        no_senders_timeout_handle_container.get_or_insert_with(move || {
+                            let loop_handle_container = loop_handle_container.clone();
+
+                            actix_rt::spawn(async move {
+                                actix_rt::time::sleep(no_senders_timeout).await;
+
+                                if let Some(handle) = loop_handle_container
+                                    .lock()
+                                    .expect("Unable to lock loop_handle_container")
+                                    .take()
+                                {
+                                    handle.abort();
+                                }
+                            })
+                        });
+                    } else {
+                        if let Some(handle) = no_senders_timeout_handle_container.take() {
+                            handle.abort();
+                        }
+                    }
+
                     let mut senders_to_remove = HashSet::new();
 
-                    for (index, mut sender) in locked_senders.iter_mut().enumerate() {
+                    for (index, sender) in locked_senders.iter_mut().enumerate() {
                         let result = sender.try_send(t.clone());
                         if let Err(error) = result {
                             if error.is_full() {
@@ -53,18 +84,17 @@ impl<T: Clone + 'static> Inner<T> {
                         *locked_senders = mem::take(&mut *locked_senders)
                             .into_iter()
                             .enumerate()
-                            .filter_map(|(i, sender)| {
-                                if senders_to_remove.contains(&i) {
-                                    Some(sender)
-                                } else {
-                                    None
-                                }
-                            })
+                            .filter(|(i, _)| !senders_to_remove.contains(&i))
+                            .map(|(i, sender)| sender)
                             .collect::<Vec<_>>()
                     }
                 }
             }
         });
+        let _ = loop_handle_container
+            .lock()
+            .expect("Unable to lock loop_handle_container")
+            .insert(loop_handle);
 
         Self {
             // Static
@@ -77,7 +107,7 @@ impl<T: Clone + 'static> Inner<T> {
     }
 
     pub(crate) fn create_receiver(&self) -> mpsc::Receiver<T> {
-        let (tx, rx) = mpsc::channel::<T>(self.child_sender_buffer);
+        let (tx, rx) = mpsc::channel::<T>(self.child_sender_buffer_size);
 
         self.children_senders
             .lock()

bfca6bc0ab775d92cc7e57b5dc03e8cf9718dc24
Roman L <roman.lakhtadyr@gmail.com>
2022-12-17 18:42:32 +0000

    feat(overlay): channel player

--- services/stream-overlay/src/App.js
+++ services/stream-overlay/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import {useEffect, useRef, useState} from "react";
 
 const CURRENT_TRACK_URL = (channelId) => `/radio-manager/api/pub/v0/streams/${channelId}/current-track`
+const STREAM_URL = (channelId) => `http://localhost:40003/listen/${channelId}?format=mp3_256k`
 
 function useCurrentTrack({channelId}) {
   const [currentTrack, setCurrentTrack] = useState(null);
 
   useEffect(() => {
-
     let timeoutRef = null
+    let abortControllerRef = null;
 
     const fetchCurrentTrack = async () => {
       if (timeoutRef) {
@@ -16,7 +17,11 @@ function useCurrentTrack({channelId}) {
       }
 
       try {
-        const response = await fetch(CURRENT_TRACK_URL(channelId));
+        abortControllerRef = new AbortController();
+        const response = await fetch(CURRENT_TRACK_URL(channelId), {
+          signal: abortControllerRef.signal
+        });
+        abortControllerRef = null
         const body = await response.json();
         const nextFetchIn = Math.min(body.data.duration - body.data.position, 5000);
 
@@ -34,30 +39,48 @@ function useCurrentTrack({channelId}) {
       if (timeoutRef) {
         clearTimeout(timeoutRef)
       }
+      if (abortControllerRef) {
+        abortControllerRef.abort();
+      }
     }
   }, [channelId, setCurrentTrack]);
 
   return currentTrack;
 }
 
+function usePlayer({channelId}) {
+  useEffect(() => {
+    const player = new Audio();
+
+    player.src = STREAM_URL(channelId);
+    player.play();
+
+    return () => {
+      player.src = null;
+    }
+  }, [channelId]);
+}
+
 function App({channelId}) {
-  const currentTrack = useCurrentTrack({channelId})
+  usePlayer({channelId});
+
+  const currentTrack = useCurrentTrack({channelId});
 
   return (<div className="App">
-      <div className="Overlay">
-        <div className="Overlay-cover">
-          <img alt="cover" src="https://fakeimg.pl/150x150/"/>
+    <div className="Overlay">
+      <div className="Overlay-cover">
+        <img alt="cover" src="https://fakeimg.pl/150x150/"/>
+      </div>
+      <div className="Overlay-track">
+        <div className="Overlay-track_title">
+          {currentTrack?.title}
         </div>
-        <div className="Overlay-track">
-          <div className="Overlay-track_title">
-            {currentTrack?.title}
-          </div>
-          <div className="Overlay-track_artist">
-            {currentTrack?.artist}
-          </div>
+        <div className="Overlay-track_artist">
+          {currentTrack?.artist}
         </div>
       </div>
-    </div>);
+    </div>
+  </div>);
 }
 
 export default App;

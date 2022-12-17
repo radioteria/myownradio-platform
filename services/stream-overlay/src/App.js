8478d09d13ba2df88afd79bcd213851ab2d55929
Roman L <roman.lakhtadyr@gmail.com>
2022-12-17 16:00:01 +0000

    fix(overlay): implement basic overlay page

--- services/stream-overlay/src/App.js
+++ services/stream-overlay/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Overlay">
+          <div className="Overlay-cover">
+              <img alt="cover" src="https://fakeimg.pl/150x150/" />
+          </div>
+          <div className="Overlay-track">
+              <div className="Overlay-track_title">
+                  Back to France
+              </div>
+              <div className="Overlay-track_artist">
+                  Mike Oldfield
+              </div>
+          </div>
+      </div>
     </div>
   );
 }

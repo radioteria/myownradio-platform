a7e90824cc9127a751c328e21b0958739e8d213d
Roman L <roman.lakhtadyr@gmail.com>
2023-01-13 16:06:00 +0000

    feat(utils): implement timesync_channel

--- services/radio-streamer/src/stream/mod.rs
+++ services/radio-streamer/src/stream/mod.rs
@@ -6,6 +6,7 @@ pub(crate) mod player_loop;
 pub mod stream;
 pub mod streams_registry;
 mod timed_channel;
+mod timesync_channel;
 pub(crate) mod types;
 
 pub(crate) use ffmpeg::{build_ffmpeg_decoder, build_ffmpeg_encoder, DecoderOutput, EncoderError};

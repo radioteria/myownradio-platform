3b40b61fb3e90bd8a1535246601bb735dd93f83e
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-01-17 23:58:22 +0000

    refactor(radio-streamer): improve comment

--- services/radio-streamer/src/stream/constants.rs
+++ services/radio-streamer/src/stream/constants.rs
@@ -8,4 +8,7 @@ pub(crate) const AUDIO_CHANNELS_NUMBER: usize = 2;
 
 /// Duration of time in which the stream player sends initial
 /// buffers in real-time before slowing down to its normal real-time pace.
+///
+/// This allows players to quickly fulfill their initial buffers and start playback immediately,
+/// rather than waiting for a longer duration of time to gather enough data before starting playback.
 pub(crate) const REALTIME_STARTUP_BUFFER_TIME: Duration = Duration::from_millis(2500);

dd1a175400b1531527c749f0b44af99fa1b9f4d6
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-04-12 14:54:29 +0100

    feat(decoder): enable OGG format support

--- services/radio-streamer/src/stream/ffmpeg/decoder.rs
+++ services/radio-streamer/src/stream/ffmpeg/decoder.rs
@@ -246,36 +246,36 @@ mod tests {
                 Duration::from_millis(2833),
                 Duration::from_millis(1500),
             ),
-            // (
-            //     "tests/fixtures/test_file.m4a",
-            //     Duration::from_millis(2854),
-            //     Duration::from_millis(0),
-            // ),
-            // (
-            //     "tests/fixtures/test_file.m4a",
-            //     Duration::from_millis(2854),
-            //     Duration::from_millis(1500),
-            // ),
-            // (
-            //     "tests/fixtures/test_file.mp3",
-            //     Duration::from_millis(2858),
-            //     Duration::from_millis(0),
-            // ),
-            // (
-            //     "tests/fixtures/test_file.mp3",
-            //     Duration::from_millis(2858),
-            //     Duration::from_millis(1500),
-            // ),
-            // (
-            //     "tests/fixtures/test_file.ogg",
-            //     Duration::from_millis(2836),
-            //     Duration::from_millis(0),
-            // ),
-            // (
-            //     "tests/fixtures/test_file.ogg",
-            //     Duration::from_millis(2836),
-            //     Duration::from_millis(1500),
-            // ),
+            (
+                "tests/fixtures/test_file.m4a",
+                Duration::from_millis(2854),
+                Duration::from_millis(0),
+            ),
+            (
+                "tests/fixtures/test_file.m4a",
+                Duration::from_millis(2854),
+                Duration::from_millis(1500),
+            ),
+            (
+                "tests/fixtures/test_file.mp3",
+                Duration::from_millis(2858),
+                Duration::from_millis(0),
+            ),
+            (
+                "tests/fixtures/test_file.mp3",
+                Duration::from_millis(2858),
+                Duration::from_millis(1500),
+            ),
+            (
+                "tests/fixtures/test_file.ogg",
+                Duration::from_millis(2834),
+                Duration::from_millis(0),
+            ),
+            (
+                "tests/fixtures/test_file.ogg",
+                Duration::from_millis(2834),
+                Duration::from_millis(1500),
+            ),
         ];
 
         for (filename, expected_duration, offset) in test_files {

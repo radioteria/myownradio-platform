fcf4e6dfea5b529d8fd909e8e09aa41a82838530
Roman L <roman.lakhtadyr@gmail.com>
2023-05-04 15:33:41 +0100

    player-loop: Implement player loop test

--- services/radio-streamer/crates/myownradio-player-loop/src/player_loop.rs
+++ services/radio-streamer/crates/myownradio-player-loop/src/player_loop.rs
@@ -2,10 +2,11 @@ use crate::running_time::RunningTime;
 use myownradio_ffmpeg_utils::{
     AudioTranscoder, AudioTranscoderCreationError, OutputFormat, Packet, TranscodeError,
 };
+use std::fmt::Debug;
 use std::time::{Duration, SystemTime};
 
 /// Defines an error that occurred while fetching now playing information.
-pub trait NowPlayingError {
+pub trait NowPlayingError: Debug {
     fn get_code(&self) -> usize;
     fn get_message(&self) -> String;
 }
@@ -28,6 +29,7 @@ pub trait NowPlayingAPIClient {
 }
 
 /// Defines the possible errors that can occur during player loop operations.
+#[derive(Debug)]
 pub enum PlayerLoopError {
     NowPlayingError(Box<dyn NowPlayingError>),
     AudioTranscoderCreationError(AudioTranscoderCreationError),
@@ -144,6 +146,114 @@ impl<API: NowPlayingAPIClient> PlayerLoop<API> {
             self.running_time.advance(&packet.pts_as_duration());
             // Update the PTS value of the packet based on the running time.
             packet.set_pts((*self.running_time.time()).into())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NowPlayingAPIClient;
+    use crate::{NowPlayingError, NowPlayingResponse, PlayerLoop, PlayerLoopError};
+    use myownradio_ffmpeg_utils::{OutputFormat, Packet};
+    use std::time::{Duration, SystemTime};
+
+    struct TestClientResponse {
+        position: Duration,
+    }
+
+    impl NowPlayingResponse for TestClientResponse {
+        fn get_url(&self) -> String {
+            String::from("tests/fixtures/sample-6s.mp3")
+        }
+
+        fn get_title(&self) -> String {
+            String::from("Sample Track")
+        }
+
+        fn get_duration(&self) -> Duration {
+            Duration::from_secs_f32(6.426122)
+        }
+
+        fn get_position(&self) -> Duration {
+            self.position
+        }
+    }
+
+    #[derive(Debug)]
+    struct TestClientError {}
+
+    impl NowPlayingError for TestClientError {
+        fn get_code(&self) -> usize {
+            todo!()
+        }
+
+        fn get_message(&self) -> String {
+            todo!()
+        }
+    }
+
+    struct TestClient {}
+
+    static START_SEEK_TOLERANCE_MS: u128 = 250000;
+
+    impl NowPlayingAPIClient for TestClient {
+        fn get_now_playing(
+            &self,
+            _channel_id: &u32,
+            time: &SystemTime,
+        ) -> Result<Box<dyn NowPlayingResponse>, Box<dyn NowPlayingError>> {
+            let duration = 6426122;
+            let mut position = time
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_micros()
+                % duration;
+
+            if position < START_SEEK_TOLERANCE_MS || duration - position < START_SEEK_TOLERANCE_MS {
+                position = 0
+            }
+
+            let position = Duration::from_micros(position as u64);
+
+            Ok(Box::new(TestClientResponse { position }))
+        }
+    }
+
+    impl Iterator for PlayerLoop<TestClient> {
+        type Item = Result<Vec<Packet>, PlayerLoopError>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            Some(self.receive_next_audio_packets())
+        }
+    }
+
+    #[test]
+    fn test_player_loop() {
+        let api_client = TestClient {};
+        let time = SystemTime::UNIX_EPOCH;
+        let player_loop = PlayerLoop::create(
+            0,
+            api_client,
+            OutputFormat::MP3 {
+                bit_rate: 128_000,
+                sampling_rate: 48_000,
+            },
+            time,
+        )
+        .expect("Unable to initialize PlayerLoop");
+        let mut iter = player_loop.take(2048);
+
+        let mut previous_pts = Duration::ZERO;
+        while let Some(Ok(pkg)) = iter.next() {
+            if pkg.is_empty() {
+                continue;
+            }
+
+            let pts = pkg.first().unwrap().pts_as_duration();
+
+            assert!(pts >= previous_pts);
+
+            previous_pts = pts;
         }
     }
 }

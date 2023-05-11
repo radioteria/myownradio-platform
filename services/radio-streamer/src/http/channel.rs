7a76707e8578336fb0d5d90219e2ed0ac52e1cb2
Roman L <roman.lakhtadyr@gmail.com>
2023-05-11 17:34:29 +0100

    player-loop: implement `get_channel_audio_stream_v3` route handler

--- services/radio-streamer/src/http/channel.rs
+++ services/radio-streamer/src/http/channel.rs
@@ -1,14 +1,18 @@
 use super::utils::icy_muxer::{IcyMuxer, ICY_METADATA_INTERVAL};
 use crate::audio_formats::AudioFormats;
+use crate::backend_client::{BackendClient, MorBackendClientError, NowPlaying};
 use crate::config::Config;
 use crate::stream::{StreamCreateError, StreamMessage, StreamsRegistry, StreamsRegistryExt};
-use actix_web::web::{Data, Query};
+use actix_web::web::{Bytes, Data, Query};
 use actix_web::{get, web, HttpRequest, HttpResponse, Responder};
 use futures::channel::mpsc;
+use futures::executor::{block_on, LocalSpawner};
 use futures::StreamExt;
+use myownradio_player_loop::{NowPlayingClient, NowPlayingError, NowPlayingResponse, PlayerLoop};
 use serde::Deserialize;
 use slog::{debug, error, warn, Logger};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, SystemTime};
 
 #[get("/active")]
 pub(crate) async fn get_active_channel_ids(
@@ -163,5 +167,133 @@ pub(crate) async fn get_channel_audio_stream_v2(
         } else {
             response.streaming::<_, actix_web::Error>(response_receiver.map(Ok))
         }
+    }
+}
+
+impl NowPlayingResponse for NowPlaying {
+    fn curr_url(&self) -> String {
+        self.current_track.url.clone()
+    }
+
+    fn curr_title(&self) -> String {
+        self.current_track.title.clone()
+    }
+
+    fn curr_duration(&self) -> Duration {
+        self.current_track.duration
+    }
+
+    fn curr_position(&self) -> Duration {
+        self.current_track.offset
+    }
+
+    fn next_url(&self) -> String {
+        self.next_track.url.clone()
+    }
+
+    fn next_title(&self) -> String {
+        self.next_track.title.clone()
+    }
+
+    fn next_duration(&self) -> Duration {
+        self.next_track.duration
+    }
+}
+
+impl NowPlayingError for MorBackendClientError {}
+
+impl NowPlayingClient for BackendClient {
+    fn get_now_playing(
+        &self,
+        channel_id: &u32,
+        time: &SystemTime,
+    ) -> Result<Box<dyn NowPlayingResponse>, Box<dyn NowPlayingError>> {
+        let channel_id = *channel_id as usize;
+        let mut lp = futures::executor::LocalPool::new();
+
+        let future = BackendClient::get_now_playing(self, &channel_id, time);
+
+        lp.run_until(future)
+            .map(|now| Box::new(now) as Box<dyn NowPlayingResponse>)
+            .map_err(|err| Box::new(err) as Box<dyn NowPlayingError>)
+    }
+}
+
+#[get("/v3/listen/{channel_id}")]
+pub(crate) async fn get_channel_audio_stream_v3(
+    request: HttpRequest,
+    channel_id: web::Path<u32>,
+    query_params: Query<GetChannelAudioStreamQueryParams>,
+    logger: Data<Arc<Logger>>,
+    client: Data<Arc<BackendClient>>,
+) -> impl Responder {
+    let channel_id = channel_id.into_inner();
+    let format = query_params
+        .into_inner()
+        .format
+        .and_then(|format| AudioFormats::from_string(&format))
+        .unwrap_or_default();
+    let initial_time = SystemTime::now();
+    let content_type = format.content_type;
+
+    let result = PlayerLoop::create(
+        channel_id,
+        BackendClient::clone(&client),
+        format.into(),
+        initial_time,
+    );
+
+    let player_loop = Arc::new(Mutex::new(match result {
+        Ok(player_loop) => player_loop,
+        Err(error) => {
+            error!(logger, "Unable to create player loop"; "error" => ?error);
+            return HttpResponse::ServiceUnavailable().finish();
+        }
+    }));
+
+    let (response_sender, response_receiver) = mpsc::channel(512);
+
+    actix_rt::spawn({
+        let player_loop = player_loop;
+        let mut response_sender = response_sender;
+
+        async move {
+            while let Ok(packets) = actix_rt::task::spawn_blocking({
+                let player_loop = player_loop.clone();
+
+                move || player_loop.lock().unwrap().receive_next_audio_packets()
+            })
+            .await
+            .unwrap()
+            {
+                eprintln!("next");
+
+                for packet in packets {
+                    let bytes = Bytes::copy_from_slice(&packet.data());
+                    match response_sender.try_send(Ok(bytes)) {
+                        Err(error) if error.is_disconnected() => {
+                            // Disconnected: drop the receiver
+                            break;
+                        }
+                        Err(error) if error.is_full() => {
+                            // Buffer is full: skip the remaining bytes
+                        }
+                        Err(error) => {
+                            warn!(logger, "Unable to send bytes to the client"; "error" => ?error);
+                            // Unexpected error: drop the receiver
+                            break;
+                        }
+                        Ok(_) => (),
+                    }
+                }
+            }
+        }
+    });
+
+    {
+        let mut response = HttpResponse::Ok();
+
+        response.content_type(content_type).force_close();
+        response.streaming::<_, actix_web::Error>(response_receiver)
     }
 }

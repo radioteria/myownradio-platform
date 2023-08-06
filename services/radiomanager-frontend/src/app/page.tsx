06f59791452091920b13a12e5a2c819e774358f0
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-08-06 17:49:38 +0100

    radiomanager-frontend: Improve layout sizes and colors

--- services/radiomanager-frontend/src/app/page.tsx
+++ services/radiomanager-frontend/src/app/page.tsx
@@ -4,14 +4,22 @@ export default function Home() {
   return (
     <main className={cn("flex h-screen")}>
       <div className={cn("flex-1 flex flex-col overflow-hidden")}>
-        <nav className={cn("flex h-24 bg-slate-800 text-gray-100")}>
+        <nav className={cn("flex h-16 bg-slate-800 text-gray-100")}>
           <div className={cn("flex")}>TODO: Menu</div>
         </nav>
         <div className={cn("flex h-full")}>
-          <aside className={cn("flex w-64 h-full bg-slate-300")}>
+          <aside
+            className={cn(
+              "flex w-64 h-full from-gray-300 to-gray-100 bg-gradient-to-b",
+            )}
+          >
             TODO: Sidebar
           </aside>
-          <div className={cn("flex flex-col w-full")}>TODO: Content</div>
+          <div className={cn("flex flex-col w-full")}>
+            <div>
+              <h3 className={cn("text-xl")}>All tracks</h3>
+            </div>
+          </div>
         </div>
       </div>
     </main>

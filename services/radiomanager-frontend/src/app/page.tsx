bc17cc9585c42d9e3c0d26c0fc1874a8cce3ae5d
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-08-06 17:34:04 +0100

    radiomanager-frontend: Add `classnames`

--- services/radiomanager-frontend/src/app/page.tsx
+++ services/radiomanager-frontend/src/app/page.tsx
@@ -1,15 +1,17 @@
+import cn from "classnames";
+
 export default function Home() {
   return (
-    <main className={"flex h-screen"}>
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <nav className="flex h-24 bg-slate-800 text-gray-100">
-          <div className="flex">TODO: Menu</div>
+    <main className={cn("flex h-screen")}>
+      <div className={cn("flex-1 flex flex-col overflow-hidden")}>
+        <nav className={cn("flex h-24 bg-slate-800 text-gray-100")}>
+          <div className={cn("flex")}>TODO: Menu</div>
         </nav>
-        <div className={"flex h-full"}>
-          <aside className={"flex w-64 h-full bg-slate-300"}>
+        <div className={cn("flex h-full")}>
+          <aside className={cn("flex w-64 h-full bg-slate-300")}>
             TODO: Sidebar
           </aside>
-          <div className={"flex flex-col w-full"}>TODO: Content</div>
+          <div className={cn("flex flex-col w-full")}>TODO: Content</div>
         </div>
       </div>
     </main>

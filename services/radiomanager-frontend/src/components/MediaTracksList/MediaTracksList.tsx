1ab0fa86945e751c81297f7415df2305218c46a6
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-08-09 17:07:29 +0100

    radiomanager-frontend: Improve MediaTracksList.tsx

--- services/radiomanager-frontend/src/components/MediaTracksList/MediaTracksList.tsx
+++ services/radiomanager-frontend/src/components/MediaTracksList/MediaTracksList.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames'
 import { UserTrack } from '@/api.types'
+import { Duration } from '@/components/Duration/Duration'
 
 interface Props {
   tracks: readonly UserTrack[]
@@ -9,10 +10,27 @@ interface Props {
 export const MediaTracksList: React.FC<Props> = ({ tracks, tracksCount }) => {
   return (
     <section>
-      <h3 className={cn('text-xl')}>All tracks ({tracksCount})</h3>
-      <ul>
+      <ul className={'mt-8'}>
+        <li className="flex text-gray-600">
+          <div className="p-2 w-8 flex-shrink-0"></div>
+          <div className="p-2 w-8 flex-shrink-0"></div>
+          <div className="p-2 w-full">Title</div>
+          <div className="p-2 w-full">Artist</div>
+          <div className="p-2 w-full">Album</div>
+          <div className="p-2 w-20 flex-shrink-0 text-right">⏱</div>
+        </li>
+
         {tracks.map((track) => (
-          <li key={track.tid}>{track.title}</li>
+          <li key={track.tid} className={'flex border-gray-800'}>
+            <div className="p-3 w-8 flex-shrink-0">▶️</div>
+            <div className="p-3 w-8 flex-shrink-0">❤️</div>
+            <div className="p-3 w-full">{track.title || track.filename}</div>
+            <div className="p-3 w-full">{track.artist}</div>
+            <div className="p-3 w-full">{track.album}</div>
+            <div className="p-3 w-20 flex-shrink-0 text-right">
+              <Duration millis={track.duration} />
+            </div>
+          </li>
         ))}
       </ul>
     </section>

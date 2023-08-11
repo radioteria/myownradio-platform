c4029b11b43213938f71e2bd333acefda656e4c6
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-08-12 00:39:50 +0100

    radiomanager-frontend: Sidebar improvements

--- services/radiomanager-frontend/src/components/Sidebar/Sidebar.tsx
+++ services/radiomanager-frontend/src/components/Sidebar/Sidebar.tsx
@@ -4,15 +4,21 @@ import Link from 'next/link'
 
 interface Props {
   channels: readonly UserStream[]
+  channelId: number
 }
 
-export const Sidebar: React.FC<Props> = ({ channels }) => {
+export const Sidebar: React.FC<Props> = ({ channels, channelId }) => {
   return (
     <div>
-      <h3 className={cn('text-xl')}>All Channels ({channels.length})</h3>
+      <h3 className={cn('text-lg p-4')}>Channels</h3>
       <ul>
         {channels.map((stream) => (
-          <li key={stream.sid}>
+          <li
+            key={stream.sid}
+            className={cn('px-4 py-2', 'text-ellipsis overflow-hidden', {
+              'bg-slate-500 text-gray-50': stream.sid === channelId,
+            })}
+          >
             <Link href={`/c/${stream.sid}`}>{stream.name}</Link>
           </li>
         ))}

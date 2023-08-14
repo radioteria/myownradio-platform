c2f1cb43721203378d50578aeeb2167dab8a1e0c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-08-14 18:23:19 +0100

    radiomanager-frontend: improve useNowPlaying hook

--- services/radiomanager-frontend/src/components/StreamOverlay/StreamOverlay.tsx
+++ services/radiomanager-frontend/src/components/StreamOverlay/StreamOverlay.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+// import { useNowPlaying } from '@/hooks/useNowPlaying'
+
 interface Props {
   readonly channelId: number
 }
@@ -6,6 +10,7 @@ export const StreamOverlay: React.FC<Props> = ({ channelId }) => {
   // TODO Connect to WS to listen channel events
   // TODO Connect to scheduler to get now-playing data
   // TODO Integrate audio player to listen to audio
+  // const { nowPlaying } = useNowPlaying(channelId)
 
   return (
     <div className={'bg-black aspect-video text-white flex items-center justify-center rounded-lg'}>

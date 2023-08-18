445dacece36f19f978cf3b7c888e7197fce66135
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-08-19 00:36:54 +0100

    radiomanager-frontend: Introduce method for selecting tracks in the list

--- services/radiomanager-frontend/src/components/common/TrackList/TrackList.tsx
+++ services/radiomanager-frontend/src/components/common/TrackList/TrackList.tsx
@@ -1,5 +1,6 @@
 import { TrackItem, CurrentTrack } from './types'
 import { TrackListItem } from '@/components/common/TrackList/TrackListItem'
+import { useEffect, useState } from 'react'
 
 interface ListItem {
   track: TrackItem
@@ -11,7 +12,49 @@ interface Props {
   currentTrack: CurrentTrack | null
 }
 
+const mapTracksToListItems = (tracks: readonly TrackItem[]) => {
+  return tracks.map((track) => ({ track, isSelected: false }))
+}
+
 export const TrackList: React.FC<Props> = ({ tracks, currentTrack }) => {
+  const [listItems, setListItems] = useState(mapTracksToListItems(tracks))
+  const [selectionCursor, setSelectionCursor] = useState<null | number>(null)
+
+  // Reset selection on tracks list update.
+  // TODO Preserve selection
+  useEffect(() => setListItems(mapTracksToListItems(tracks)), [tracks])
+
+  const selectTrack = (selectIndex: number) => {
+    setListItems((items) =>
+      items.map((item, index) => {
+        return selectIndex === index ? { ...item, isSelected: true } : item
+      }),
+    )
+  }
+
+  const discardTrack = (discardIndex: number) => {
+    setListItems((items) =>
+      items.map((item, index) => {
+        return discardIndex === index ? { ...item, isSelected: false } : item
+      }),
+    )
+  }
+
+  const selectTo = (endIndex: number) => {
+    const startIndex = selectionCursor ?? 0
+
+    setListItems((items) =>
+      items.map((item, index) => {
+        return startIndex <= index && index <= endIndex ? { ...item, isSelected: true } : item
+      }),
+    )
+  }
+
+  const resetSelection = () => {
+    setListItems((items) => items.map(({ track }) => ({ track, isSelected: false })))
+    setSelectionCursor(null)
+  }
+
   return (
     <ul>
       <li className="flex text-gray-500">
@@ -22,11 +65,11 @@ export const TrackList: React.FC<Props> = ({ tracks, currentTrack }) => {
         <div className="pl-2 pr-4 py-4 w-10 flex-shrink-0 text-right" />
       </li>
 
-      {tracks.map((track, index) => {
+      {listItems.map((item, index) => {
         return (
           <TrackListItem
             key={index}
-            track={track}
+            track={item.track}
             currentTrack={currentTrack}
             index={index}
             onRemoveFromLibrary={() => {}}

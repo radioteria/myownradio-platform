3d8e1c108f650904c9727c0c7b24b32fd8b99235
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-09-14 11:00:02 +0100

    Fix INITIAL_AUDIO_TRACKS_TO_LOAD = 50

--- services/radiomanager-frontend/src/constants.ts
+++ services/radiomanager-frontend/src/constants.ts
@@ -1,2 +1,2 @@
 // Number of audio tracks to be initially loaded when the page loads on the server
-export const INITIAL_AUDIO_TRACKS_TO_LOAD: number = 5
+export const INITIAL_AUDIO_TRACKS_TO_LOAD: number = 50

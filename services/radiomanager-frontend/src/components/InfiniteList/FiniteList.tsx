2e86094d1a668163d3df5e526b610d618bef2e28
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-09-18 20:12:09 +0100

    radiomanager-frontend: Implement `FiniteList` component

---
+++ services/radiomanager-frontend/src/components/InfiniteList/FiniteList.tsx
@@ -0,0 +1,83 @@
+import { OnReachTrigger } from './OnReachTrigger'
+import { useCallback, useEffect, useState } from 'react'
+
+interface ListItem {}
+
+interface LoadMoreItemsResult<Item extends ListItem> {
+  items: readonly Item[]
+  totalCount: number
+}
+
+interface LoadRequest {
+  readonly startIndex: number
+  readonly endIndex: number
+}
+
+interface Props<Item extends ListItem> {
+  readonly items: readonly (Item | null)[]
+  readonly getItemKey: (item: Item | null, index: number) => React.Key
+
+  readonly renderSkeleton: (index: number) => React.ReactNode
+  readonly renderItem: (item: Item, index: number) => React.ReactNode
+
+  readonly loadMoreItems: (
+    startIndex: number,
+    endIndex: number,
+    signal: AbortSignal,
+  ) => Promise<LoadMoreItemsResult<Item>>
+}
+
+export function FiniteList<Item extends ListItem>({
+  items,
+  renderSkeleton,
+  renderItem,
+  getItemKey,
+  loadMoreItems,
+}: Props<Item>) {
+  const [isLoading, setIsLoading] = useState(false)
+  const [loadRequest, setLoadRequest] = useState<null | LoadRequest>(null)
+
+  // Trigger "loadMoreItems"
+  const handleOnReach = (index: number) => {
+    if (isLoading) return
+
+    if (items[index] === null) {
+      const startIndex = Math.max(0, index - 25)
+      const endIndex = index + 25
+
+      // Load more data
+      setLoadRequest({ startIndex, endIndex })
+      setIsLoading(true)
+    }
+  }
+
+  // Handle "loadMoreItems"
+  useEffect(() => {
+    if (!loadRequest) return
+
+    const abortController = new AbortController()
+
+    loadMoreItems(loadRequest.startIndex, loadRequest.endIndex, abortController.signal).finally(
+      () => {
+        setIsLoading(false)
+        setLoadRequest(null)
+      },
+    )
+
+    return () => {
+      abortController.abort()
+    }
+  }, [loadRequest, loadMoreItems])
+
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={getItemKey(item, index)}>
+          <OnReachTrigger onReach={handleOnReach.bind(undefined, index)}>
+            {item === null ? renderSkeleton(index) : renderItem(item, index)}
+          </OnReachTrigger>
+        </li>
+      ))}
+    </ul>
+  )
+}

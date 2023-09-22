ef524a8a83ead85de4dd0422eb256ebe145d0f25
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-09-22 17:18:04 +0100

    Use dark mode colors in skeleton

--- services/radiomanager-frontend/src/components/common/TrackList/ListItemSkeleton.tsx
+++ services/radiomanager-frontend/src/components/common/TrackList/ListItemSkeleton.tsx
@@ -9,24 +9,32 @@ export const ListItemSkeleton: React.FC<Props> = () => {
     <div className="flex items-center border-gray-800 h-12 relative cursor-pointer select-none">
       <div className="p-2 pl-4 w-14 flex-shrink-0 z-10 text-right">
         <div
-          className={cn('w-4 h-5 bg-gray-300 rounded-md inline-block ', styles.skeletonAnimation)}
+          className={cn(
+            'w-6 h-5 bg-morblue-800 rounded-md inline-block ',
+            styles.skeletonAnimation,
+          )}
         />
       </div>
       <div className="p-2 w-full z-10 min-w-0">
-        <div className={cn('w-[75%] h-4 bg-gray-300 rounded-md mb-1', styles.skeletonAnimation)} />
-        <div className={cn('w-[50%] h-3 bg-gray-300 rounded-md', styles.skeletonAnimation)} />
+        <div
+          className={cn('w-[75%] h-4 bg-morblue-800 rounded-md mb-1', styles.skeletonAnimation)}
+        />
+        <div className={cn('w-[50%] h-3 bg-morblue-800 rounded-md', styles.skeletonAnimation)} />
       </div>
       <div className="px-2 py-4 w-full hidden xl:block">
         <div
           className={cn(
-            'w-[75%] h-4 bg-gray-300 rounded-md inline-block',
+            'w-[75%] h-4 bg-morblue-800 rounded-md inline-block',
             styles.skeletonAnimation,
           )}
         />
       </div>
       <div className="p-2 w-20 flex-shrink-0 text-right z-10">
         <div
-          className={cn('w-10 h-5 bg-gray-300 rounded-md inline-block', styles.skeletonAnimation)}
+          className={cn(
+            'w-10 h-5 bg-morblue-800 rounded-md inline-block',
+            styles.skeletonAnimation,
+          )}
         />
       </div>
       <div className="pl-2 pr-4 py-4 w-10 flex-shrink-0 text-right" />

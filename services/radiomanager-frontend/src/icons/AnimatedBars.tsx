66c6b9f6138642dc3408c153d431a14a932ee4a5
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-09-30 14:47:12 +0100

    Optimize AnimatedBars animation

--- services/radiomanager-frontend/src/icons/AnimatedBars.tsx
+++ services/radiomanager-frontend/src/icons/AnimatedBars.tsx
@@ -12,14 +12,15 @@ export const AnimatedBars: React.FC<Props> = ({ size }) => {
         xmlns="http://www.w3.org/2000/svg"
         fill={'currentcolor'}
       >
-        <rect className="bar bar1" x="0" y="16" width="3" height="0" />
-        <rect className="bar bar2" x="4" y="16" width="3" height="0" />
-        <rect className="bar bar3" x="8" y="16" width="3" height="0" />
-        <rect className="bar bar4" x="12" y="16" width="3" height="0" />
+        <rect className="bar bar1" x="0" y="16" width="3" />
+        <rect className="bar bar2" x="4" y="16" width="3" />
+        <rect className="bar bar3" x="8" y="16" width="3" />
+        <rect className="bar bar4" x="12" y="16" width="3" />
       </svg>
       <style jsx>{`
         .bar {
           animation: growBar 0.35s infinite alternate;
+          height: 100%;
         }
 
         .bar1 {
@@ -40,12 +41,10 @@ export const AnimatedBars: React.FC<Props> = ({ size }) => {
 
         @keyframes growBar {
           from {
-            height: 0;
-            y: 16px;
+            transform: translateY(0);
           }
           to {
-            height: 100%;
-            y: 0px;
+            transform: translateY(-16px);
           }
         }
       `}</style>

9003a4d158cad36737e1552b8577b786eaeaedac
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-15 17:19:50 +0100

    Add /users/{user_id} path prefix

--- services/radiomanager-web-egress-controller/src/types.rs
+++ services/radiomanager-web-egress-controller/src/types.rs
@@ -1,11 +1,7 @@
-use actix_http::{BoxedPayloadStream, Payload};
-use actix_web::error::ErrorBadRequest;
-use actix_web::{FromRequest, HttpRequest};
-use futures::future::{err, ok, Ready};
+use serde::Deserialize;
 use std::ops::Deref;
-use tracing::warn;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Deserialize)]
 pub(crate) struct UserId(i32);
 
 impl Deref for UserId {
@@ -19,27 +15,5 @@ impl Deref for UserId {
 impl From<i32> for UserId {
     fn from(id: i32) -> Self {
         UserId(id)
-    }
-}
-
-impl FromRequest for UserId {
-    type Error = actix_web::Error;
-    type Future = Ready<Result<Self, Self::Error>>;
-
-    fn from_request(req: &HttpRequest, _payload: &mut Payload<BoxedPayloadStream>) -> Self::Future {
-        ok(UserId::from(
-            match req.headers().get("user-id").and_then(|header| {
-                header
-                    .to_str()
-                    .ok()
-                    .and_then(|header| header.parse::<i32>().ok())
-            }) {
-                Some(user_id) => user_id,
-                _ => {
-                    warn!("Bad request: no user-id header");
-                    return err(ErrorBadRequest(String::new()));
-                }
-            },
-        ))
     }
 }

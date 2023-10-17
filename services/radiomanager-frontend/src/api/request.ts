3afc7da8b29e03a16a9f38a5ddea1f2562e274be
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-17 08:33:25 +0100

    Introduce Request class

--- services/radiomanager-frontend/src/api/request.ts
+++ services/radiomanager-frontend/src/api/request.ts
@@ -1,3 +1,68 @@
+import z from 'zod'
+
+export class Request {
+  private readonly url: URL
+  private readonly headers: [string, string][] = []
+
+  private method = 'GET'
+  private signal: AbortSignal | null = null
+  private body: BodyInit | null = null
+
+  constructor(url: string | URL) {
+    this.url = typeof url === 'string' ? new URL(url) : url
+  }
+
+  public setMethod(method: string): Request {
+    this.method = method
+    return this
+  }
+
+  public setQueryParam(name: string, value: string): Request {
+    this.url.searchParams.set(name, value)
+    return this
+  }
+
+  public addHeader(name: string, value: string): Request {
+    this.headers.push([name, value])
+    return this
+  }
+
+  public setAbortSignal(signal: AbortSignal): Request {
+    this.signal = signal
+    return this
+  }
+
+  public setBody(body: BodyInit): Request {
+    this.body = body
+    return this
+  }
+
+  public withTokenAuth(): Request {
+    const token = new URL(window.location.href).searchParams.get('token')
+
+    if (token) {
+      this.addHeader('Authentication', `Bearer ${token}`)
+    }
+
+    return this
+  }
+
+  public fetch(): Promise<Response> {
+    return fetch(this.url, {
+      method: this.method,
+      signal: this.signal,
+      body: this.body,
+      headers: this.headers,
+    })
+  }
+
+  public fetchWithSchema<T>(schema: z.ZodType<T, any, unknown>): Promise<T> {
+    return this.fetch()
+      .then((r) => r.json())
+      .then((data) => schema.parse(data))
+  }
+}
+
 interface RequestParams {
   method?: string
   headers?: [string, string][]

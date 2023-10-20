9241773249b6faf5ab7c3e3df862448bad1347af
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-20 15:11:07 +0100

    Synchronize gradient background

--- services/radiomanager-frontend/src/views/PlayerPage/PlayerOverlay/GradientBackground/GradientBackground.tsx
+++ services/radiomanager-frontend/src/views/PlayerPage/PlayerOverlay/GradientBackground/GradientBackground.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 
 interface Props {
   readonly children?: React.ReactNode
+  readonly initialColorIndex: number
 }
 
 const colors = [
@@ -29,30 +30,17 @@ const colors = [
 
 const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)]
 
-export const GradientBackground: React.FC<Props> = ({ children }) => {
-  const [color1, setColor1] = useState(() => getRandomColor())
-  const [color2, setColor2] = useState(() => getRandomColor())
-  const [color3, setColor3] = useState(() => getRandomColor())
-  const [color4, setColor4] = useState(() => getRandomColor())
-
-  const [color5, setColor5] = useState(() => getRandomColor())
-  const [color6, setColor6] = useState(() => getRandomColor())
-  const [color7, setColor7] = useState(() => getRandomColor())
-  const [color8, setColor8] = useState(() => getRandomColor())
+export const GradientBackground: React.FC<Props> = ({ children, initialColorIndex }) => {
+  const [colorIndex1, setColorIndex1] = useState(initialColorIndex)
+  const [colorIndex2, setColorIndex2] = useState(initialColorIndex)
 
   const [activeLayer, setActiveLayer] = useState(0)
 
   useEffect(() => {
     if (activeLayer === 0) {
-      setColor1(getRandomColor())
-      setColor2(getRandomColor())
-      setColor3(getRandomColor())
-      setColor4(getRandomColor())
+      setColorIndex1((index) => (index + 1) % (colors.length - 3))
     } else {
-      setColor5(getRandomColor())
-      setColor6(getRandomColor())
-      setColor7(getRandomColor())
-      setColor8(getRandomColor())
+      setColorIndex2((index) => (index + 1) % (colors.length - 3))
     }
 
     let timeoutId = window.setTimeout(() => {
@@ -70,18 +58,18 @@ export const GradientBackground: React.FC<Props> = ({ children }) => {
         .gradient1 {
           opacity: ${activeLayer === 0 ? '1' : '0'};
           transition: opacity 45s linear;
-          background: linear-gradient(to bottom left, ${color1} 0%, transparent 75%),
-            linear-gradient(to bottom right, ${color2} 0%, transparent 75%),
-            linear-gradient(to top left, ${color3} 0%, transparent 75%),
-            linear-gradient(to top right, ${color4} 0%, transparent 75%);
+          background: linear-gradient(to bottom left, ${colors[colorIndex1]} 0%, transparent 75%),
+            linear-gradient(to bottom right, ${colors[colorIndex1 + 1]} 0%, transparent 75%),
+            linear-gradient(to top left, ${colors[colorIndex1 + 2]} 0%, transparent 75%),
+            linear-gradient(to top right, ${colors[colorIndex1 + 3]} 0%, transparent 75%);
         }
         .gradient2 {
           opacity: ${activeLayer === 1 ? '1' : '0'};
           transition: opacity 45s linear;
-          background: linear-gradient(to bottom left, ${color5} 0%, transparent 75%),
-            linear-gradient(to bottom right, ${color6} 0%, transparent 75%),
-            linear-gradient(to top left, ${color7} 0%, transparent 75%),
-            linear-gradient(to top right, ${color8} 0%, transparent 75%);
+          background: linear-gradient(to bottom left, ${colors[colorIndex2]} 0%, transparent 75%),
+            linear-gradient(to bottom right, ${colors[colorIndex2 + 1]} 0%, transparent 75%),
+            linear-gradient(to top left, ${colors[colorIndex2 + 2]} 0%, transparent 75%),
+            linear-gradient(to top right, ${colors[colorIndex2 + 3]} 0%, transparent 75%);
         }
       `}</style>
       <div className={'w-full h-full gradient1 absolute'} />

5c114c5fd85154bdf246b4677c073e47d6133ee4
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-24 18:41:04 +0100

    Revert "Refactor pulseaudio and Xvfb execution for better fault isolation"

--- services/radiomanager-web-egress-process/entrypoint.sh
+++ services/radiomanager-web-egress-process/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 APP="/radiomanager-web-egress-process"
-
-Xvfb :0 -screen 0 1280x720x30+32 & XVFB_PID=$!
-pulseaudio --disable-shm=true --realtime=true --exit-idle-time=-1 & PULSE_PID=$!
+XVFB_ARGS="-nolisten tcp -screen 0 1920x1080x30+32"
 
 trap 'pkill -INT -f "^$APP"; wait $PID' INT
 trap 'pkill -TERM -f "^$APP"; wait $PID' TERM
 trap 'pkill -USR1 -f "^$APP"; wait $PID' USR1
 
-$APP & PID=$!
+xvfb-run -a --server-args="$XVFB_ARGS" $APP & PID=$!
 
-wait $PID $XVFB_PID $PULSE_PID
+wait $PID

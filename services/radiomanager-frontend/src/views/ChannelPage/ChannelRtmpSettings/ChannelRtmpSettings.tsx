ab16936952db604dde20af7cc1912390997d0f94
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-26 14:21:30 +0100

    frontend: Improve layout of RTMP form

--- services/radiomanager-frontend/src/views/ChannelPage/ChannelRtmpSettings/ChannelRtmpSettings.tsx
+++ services/radiomanager-frontend/src/views/ChannelPage/ChannelRtmpSettings/ChannelRtmpSettings.tsx
@@ -9,40 +9,59 @@ interface Props {
 
 export const ChannelRtmpSettings: React.FC<Props> = ({ channel, onUpdateRtmpSettings }) => {
   const [rtmpUrl, setRtmpUrl] = useState(() => channel.rtmpUrl)
-  const [rtmpStreamingKey, setRtmpStreamingKey] = useState(() => channel.rtmpStreamingKey)
+  const [rtmpStreamKey, setRtmpStreamKey] = useState(() => channel.rtmpStreamingKey)
 
   const handleRtmpUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
     setRtmpUrl(event.target.value)
   }
 
   const handleRtmpStreamingKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setRtmpStreamingKey(event.target.value)
+    setRtmpStreamKey(event.target.value)
   }
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
-    onUpdateRtmpSettings(rtmpUrl, rtmpStreamingKey)
+    onUpdateRtmpSettings(rtmpUrl, rtmpStreamKey)
   }
 
   return (
-    <form className={'block'} onSubmit={handleSubmit}>
-      <div>
-        <div>URL</div>
-        <input className={'block w-full'} value={rtmpUrl} onChange={handleRtmpUrlChange} />
-      </div>
-      <div>
-        <div>Streaming Key</div>
-        <input
-          className={'block w-full'}
-          value={rtmpStreamingKey}
-          type={'password'}
-          onChange={handleRtmpStreamingKeyChange}
-        />
-      </div>
-      <div>
-        <button type={'submit'}>Save</button>
-      </div>
-    </form>
+    <div className={'bg-morblue-950 rounded-lg p-4 mt-2 w-full'}>
+      <form className={''} onSubmit={handleSubmit}>
+        <div className={'mb-4'}>
+          <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="rtmpUrl">
+            RTMP URL
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="rtmpUrl"
+            type="text"
+            placeholder="rtmp://..."
+            value={rtmpUrl}
+            onChange={handleRtmpUrlChange}
+          />
+        </div>
+        <div className={'mb-6'}>
+          <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="streamKey">
+            Stream Key
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id={'streamKey'}
+            value={rtmpStreamKey}
+            type={'password'}
+            onChange={handleRtmpStreamingKeyChange}
+          />
+        </div>
+        <div className="flex items-center justify-end">
+          <button
+            className="bg-blue-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
--- services/radiomanager-frontend/tailwind.config.js
+++ services/radiomanager-frontend/tailwind.config.js
@@ -26,6 +26,15 @@ module.exports = {
         'morblue-900': '#202434',
         'morblue-950': '#1e202d',
         'morblue-1000': '#13141c',
+        'morgreen-50': '#D7EBBD',
+        'morgreen-100': '#C3E09C',
+        'morgreen-200': '#AFD67B',
+        'morgreen-300': '#9BCC5A',
+        'morgreen-400': '#7CA348',
+        'morgreen-500': '#5D7A36',
+        'morgreen-600': '#3E5224',
+        'morgreen-700': '#2E3D1B',
+        'morgreen-800': '#202A13',
       },
       gridTemplateColumns: {
         'playlist-item':

a1e544214894a944b6599b57d6d5220174a25f30
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-31 15:03:09 +0000

    feat(counter): Add unique `stream_id` constraint to `outgoing_streams`

---
+++ migrations/20231031150001_add_unique_index_for_stream_id.down.sql
@@ -0,0 +1,2 @@
+alter table `mor`.`outgoing_streams` drop constraint outgoing_streams_stream_id;
+
---
+++ migrations/20231031150001_add_unique_index_for_stream_id.up.sql
@@ -0,0 +1,1 @@
+alter table `mor`.`outgoing_streams` add constraint outgoing_streams_stream_id unique (stream_id);

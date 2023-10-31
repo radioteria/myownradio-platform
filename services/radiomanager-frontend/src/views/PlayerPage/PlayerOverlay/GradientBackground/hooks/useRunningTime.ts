7ed2f10b0159509927289b5e80c1b07d9c0bb092
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-31 21:29:23 +0000

    feat(gradient): Implement `useRunningTime`

---
+++ services/radiomanager-frontend/src/views/PlayerPage/PlayerOverlay/GradientBackground/hooks/useRunningTime.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+export const useRunningTime = (initialTimeMillis: number, paused: boolean): number => {
+  const [time, setTime] = useState(initialTimeMillis)
+
+  useEffect(() => {
+    setTime(initialTimeMillis)
+
+    if (paused) {
+      return
+    }
+
+    const start = performance.now()
+    const intervalId = window.setInterval(() => {
+      const delta = performance.now() - start
+      setTime(initialTimeMillis + delta)
+    }, 500)
+
+    return () => {
+      window.clearInterval(intervalId)
+    }
+  }, [initialTimeMillis, paused])
+
+  return time
+}

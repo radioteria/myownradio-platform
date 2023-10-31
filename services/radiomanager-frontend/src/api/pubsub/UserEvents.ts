689f04c92a2dac6321042668d794d61a83277be0
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-10-31 21:21:51 +0000

    feat(preview): Add temporarily added outgoing stream events

--- services/radiomanager-frontend/src/api/pubsub/UserEvents.ts
+++ services/radiomanager-frontend/src/api/pubsub/UserEvents.ts
@@ -2,6 +2,10 @@ import z from 'zod'
 
 export enum UserEventType {
   RestartChannel = 'RestartChannel',
+  OutgoingStreamStarted = 'OutgoingStreamStarted',
+  OutgoingStreamStats = 'OutgoingStreamStats',
+  OutgoingStreamFinished = 'OutgoingStreamFinished',
+  OutgoingStreamError = 'OutgoingStreamError',
 }
 
 const RestartChannelUserEventSchema = z.object({
@@ -9,6 +13,34 @@ const RestartChannelUserEventSchema = z.object({
   channelId: z.number(),
 })
 
-export const UserEventSchema = RestartChannelUserEventSchema
+const OutgoingStreamStartedUserEventSchema = z.object({
+  channelId: z.number(),
+  eventType: z.literal(UserEventType.OutgoingStreamStarted),
+})
+
+const OutgoingStreamStatsUserEventSchema = z.object({
+  channelId: z.number(),
+  byteCount: z.number(),
+  timePosition: z.number(),
+  eventType: z.literal(UserEventType.OutgoingStreamStats),
+})
+
+const OutgoingStreamFinishedUserEventSchema = z.object({
+  channelId: z.number(),
+  eventType: z.literal(UserEventType.OutgoingStreamFinished),
+})
+
+const OutgoingStreamErrorUserEventSchema = z.object({
+  channelId: z.number(),
+  eventType: z.literal(UserEventType.OutgoingStreamError),
+})
+
+export const UserEventSchema = z.union([
+  RestartChannelUserEventSchema,
+  OutgoingStreamStartedUserEventSchema,
+  OutgoingStreamStatsUserEventSchema,
+  OutgoingStreamFinishedUserEventSchema,
+  OutgoingStreamErrorUserEventSchema,
+])
 
 export type UserEvent = z.TypeOf<typeof UserEventSchema>

15f8b9efbfdcbdbcca25915370d3b8ff1084bb56
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-10-23 18:00:57 +0100

    feat(radio-manager-backend): Implement `get_file_contents` in the `LocalFileSystem`

--- services/radio-manager-backend/src/storage/fs/local.rs
+++ services/radio-manager-backend/src/storage/fs/local.rs
@@ -1,6 +1,10 @@
 use crate::storage::fs::FileSystem;
 use crate::utils::TeeResultUtils;
 use async_trait::async_trait;
+use futures::SinkExt;
+use std::io::Read;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 use tracing::error;
 
 #[derive(Clone)]
@@ -28,5 +32,46 @@ impl FileSystem for LocalFileSystem {
         .tee_err(|error| error!(?error, "Unable to delete file from local file system"))?;
 
         Ok(())
+    }
+
+    async fn get_file_contents(
+        &self,
+        path: &str,
+    ) -> std::io::Result<futures::channel::mpsc::Receiver<Vec<u8>>> {
+        let (tx, rx) = futures::channel::mpsc::channel(0);
+
+        let root_path = self.root_path.clone();
+        let path = path.to_string();
+
+        let file = File::open(format!("{}/{}", root_path, path)).await?;
+
+        actix_rt::spawn(async move {
+            let mut tx = tx;
+            let mut file = file;
+
+            let mut buffer = [0u8; 8192];
+
+            loop {
+                let size = match file.read(&mut buffer).await {
+                    Ok(size) => size,
+                    Err(error) => {
+                        error!(?error, "Error reading chunk from file");
+                        return;
+                    }
+                };
+
+                if size == 0 {
+                    // EOF reached
+                    break;
+                }
+
+                if let Err(error) = tx.send(buffer[..size].to_vec()).await {
+                    error!(?error, "Error sending chunk through channel");
+                    return;
+                }
+            }
+        });
+
+        Ok(rx)
     }
 }
